2f59f5a86e830856eb9472eb508c93546e05f21c
Vsevolod Stakhov <vsevolod@rambler-co.ru>
2009-07-03 19:51:05 +0400

    * If tag attribute value empty do not assume it as url

--- src/html.c
+++ src/html.c
@@ -325,6 +325,10 @@ parse_tag_url (struct worker_task *task, struct mime_text_part *part, tag_id_t i
 			c++;
 		}
 
+		if (len == 0) {
+			return;
+		}
+
 		url_text = memory_pool_alloc (task->task_pool, len + 1);
 		g_strlcpy (url_text, c, len + 1);
 		url = memory_pool_alloc (task->task_pool, sizeof (struct uri));
