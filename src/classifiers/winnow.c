95133a591e7f09a0601b34e8c436cd3c3483af37
cebka@lenovo-laptop <cebka@lenovo-laptop>
2010-01-14 14:26:23 +0300

    * Forgotten call of normalizer function

--- src/classifiers/winnow.c
+++ src/classifiers/winnow.c
@@ -170,6 +170,9 @@ winnow_classify (struct classifier_ctx *ctx, statfile_pool_t * pool, GTree * inp
 
 		if (data.count != 0) {
 			res = data.sum / data.count;
+            if (st->normalizer != NULL) {
+                res = st->normalizer (res, st->normalizer_data);
+            }
 		}
 		else {
 			res = 0;
