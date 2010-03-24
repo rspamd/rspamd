9e16e433e1386b3613ea5667b12ee14c3ef0588c
Vsevolod Stakhov <vsevolod@rambler-co.ru>
2010-03-24 19:44:52 +0300

    * Add FreeBSD port

---
+++ freebsd/port/files/patch-perl_Rspamd.pm
@@ -0,0 +1,57 @@
+--- perl/Rspamd.pm	2010-03-24 16:39:28.000000000 +0300
++++ perl/Rspamd.pm	2010-03-24 19:35:13.000000000 +0300
+@@ -1,54 +0,0 @@
+-package Mail::Rspamd;
+-
+-use 5.006001;
+-use strict;
+-use warnings;
+-
+-require Exporter;
+-
+-our @ISA = qw(Exporter);
+-our @EXPORT = qw(
+-    module_init
+-    module_reload
+-    LOG_ERROR
+-    LOG_WARNING
+-    LOG_MESSAGE
+-    LOG_INFO
+-    LOG_DEBUG
+-);
+-
+-our $VERSION = '0.0.1';
+-
+-require XSLoader;
+-XSLoader::load('Rspamd', $VERSION);
+-
+-sub module_init {
+-    my ($cfg) = @_;
+-}
+-
+-sub module_reload {
+-    my ($cfg) = @_;
+-}
+-
+-use constant LOG_ERROR          =>  1 << 3;
+-use constant LOG_WARNING        =>  1 << 4;
+-use constant LOG_MESSAGE        =>  1 << 5;
+-use constant LOG_INFO           =>  1 << 6;
+-use constant LOG_DEBUG          =>  1 << 7;
+-
+-1;
+-__END__
+-
+-=head1 NAME
+-
+-rspamd - Perl interface to the rspamd API
+-
+-=head1 SYNOPSIS
+-
+-  use rspamd;
+-
+-=head1 DESCRIPTION
+-
+-TODO: Not ready yet
+-
+-=cut
