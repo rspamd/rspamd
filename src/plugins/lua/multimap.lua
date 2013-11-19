d71d16de876580dd29ce2bf3dffae2cd9f92a752
Vsevolod Stakhov <vsevolod@highsecure.ru>
2013-11-19 18:20:24 +0000

    Remove unused function.

--- src/plugins/lua/multimap.lua
+++ src/plugins/lua/multimap.lua
@@ -13,32 +13,6 @@ local function ip_to_rbl(ip, rbl)
 	return str
 end
 
-local function split(str, delim, maxNb)
-	-- Eliminate bad cases...
-	if string.find(str, delim) == nil then
-		return { str }
-	end
-	if maxNb == nil or maxNb < 1 then
-		maxNb = 0    -- No limit
-	end
-	local result = {}
-	local pat = "(.-)" .. delim .. "()"
-	local nb = 0
-	local lastPos
-	for part, pos in string.gmatch(str, pat) do
-		nb = nb + 1
-		result[nb] = part
-		lastPos = pos
-		if nb == maxNb then break end
-	end
-	-- Handle the last field
-	if nb ~= maxNb then
-		result[nb + 1] = string.sub(str, lastPos)
-	end
-	return result
-end
-
-
 local function check_multimap(task)
 	local function multimap_rbl_cb(resolver, to_resolve, results, err, rbl)
 		task:inc_dns_req()
