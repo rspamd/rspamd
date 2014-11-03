1a1f4f0db14f506e53754102d3a62fb7f6f83c7f
Vsevolod Stakhov <vsevolod@highsecure.ru>
2014-11-03 12:47:50 +0000

    Fix stupid error for FreeBSD build.

--- src/libutil/addr.c
+++ src/libutil/addr.c
@@ -99,7 +99,7 @@ rspamd_parse_inet_address (rspamd_inet_addr_t *target, const char *src)
 		rspamd_strlcpy (target->addr.su.sun_path, src,
 				sizeof (target->addr.su.sun_path));
 #ifdef FREEBSD
-		target->addr.su.sun_len = SUN_LEN (addr);
+		target->addr.su.sun_len = SUN_LEN (&target->addr.su);
 #endif
 	}
 	else if (inet_pton (AF_INET6, src, &target->addr.s6.sin6_addr) == 1) {
