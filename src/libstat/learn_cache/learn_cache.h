b85b078b0ce485c342dc726069a2f8cb9c23411f
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-02-11 01:37:58 +0000

    Add preliminary API definition for learn cache.

---
+++ src/libstat/learn_cache/learn_cache.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015, Vsevolod Stakhov
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *	 * Redistributions of source code must retain the above copyright
+ *	   notice, this list of conditions and the following disclaimer.
+ *	 * Redistributions in binary form must reproduce the above copyright
+ *	   notice, this list of conditions and the following disclaimer in the
+ *	   documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef LEARN_CACHE_H_
+#define LEARN_CACHE_H_
+
+#include "config.h"
+#include "ucl.h"
+
+typedef enum rspamd_learn_cache_result {
+	RSPAMD_LEARN_OK = 0,
+	RSPAMD_LEARN_UNLEARN,
+	RSPAMD_LEARN_INGORE
+} rspamd_learn_t;
+
+struct rspamd_stat_cache {
+	const char *name;
+	gpointer (*init)(struct rspamd_stat_ctx *ctx, struct rspamd_config *cfg);
+	rspamd_learn_t (*process)(GTree *input, gboolean is_spam, gpointer ctx);
+	gpointer ctx;
+};
+
+#endif /* LEARN_CACHE_H_ */
