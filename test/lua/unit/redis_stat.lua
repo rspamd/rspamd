992ee0e77d502d52593be82d995d91e99afe2c78
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-02-28 23:02:27 +0000

    Add test cases.

--- test/lua/unit/redis_stat.lua
+++ test/lua/unit/redis_stat.lua
@@ -17,6 +17,8 @@ context("Redis statistics unit tests", function()
     struct rspamd_task *task,
     char **target);
   struct rspamd_task * rspamd_task_new(struct rspamd_worker *worker);
+  int rspamd_task_add_recipient (struct rspamd_task *task, const char *rcpt);
+  int rspamd_task_add_sender (struct rspamd_task *task, const char *sender);
   ]]
 
   test("Substitute redis values", function()
@@ -24,11 +26,18 @@ context("Redis statistics unit tests", function()
       {"%s%l", "symbollabel"},
       {"%s%%", "symbol%"},
       {"%s%u", "symbol"},
-      {"%s%W", "symbolW"}
+      {"%s%W", "symbolW"},
+      {"%r%l", "test@example.comlabel"},
+      {"%f-from", "test@example.com-from"}
     }
     local stcf = ffi.new("struct rspamd_statfile_config", 
       {symbol="symbol",label="label"})
     local t = ffi.C.rspamd_task_new(nil)
+    
+    assert_equal(ffi.C.rspamd_task_add_recipient(t, "Test <test@example.com>"), 1)
+    assert_equal(ffi.C.rspamd_task_add_recipient(t, "Test1 <test1@example.com>"), 1)
+    assert_equal(ffi.C.rspamd_task_add_sender(t, "Test <test@example.com>"), 1)
+    
     for _,c in ipairs(cases) do
       local pbuf = ffi.new 'char *[1]'
       local sz = ffi.C.rspamd_redis_expand_object(c[1], stcf, t, pbuf)
