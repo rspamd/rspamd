4a710a0babc7b088e15049e203a2a30185140883
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-03-11 15:18:49 +0000

    Try to fit options in 6 bits.

--- contrib/http-parser/http_parser.h
+++ contrib/http-parser/http_parser.h
@@ -129,11 +129,10 @@ enum http_parser_type { HTTP_REQUEST, HTTP_RESPONSE, HTTP_BOTH };
 enum flags
   { F_CHUNKED               = 1 << 0
   , F_CONNECTION_KEEP_ALIVE = 1 << 1
-  , F_CONNECTION_CLOSE      = 1 << 2
+  , F_SPAMC                 = 1 << 2
   , F_TRAILING              = 1 << 3
   , F_UPGRADE               = 1 << 4
   , F_SKIPBODY              = 1 << 5
-  , F_SPAMC                 = 1 << 6
   };
 
 
