6d95cb903a5ac0134d66b74ea3aed6e86717e696
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-05-11 14:17:04 +0100

    Add workaround for glib < 2.30.

--- src/libutil/util.c
+++ src/libutil/util.c
@@ -1463,6 +1463,20 @@ g_queue_clear (GQueue *queue)
 	queue->length = 0;
 }
 #endif
+#if ((GLIB_MAJOR_VERSION == 2) && (GLIB_MINOR_VERSION < 30))
+GPtrArray*
+g_ptr_array_new_full (guint reserved_size,
+		GDestroyNotify element_free_func)
+{
+	GPtrArray *array;
+
+	array = g_ptr_array_sized_new (reserved_size);
+	g_ptr_array_set_free_func (array, element_free_func);
+
+	return array;
+}
+#endif
+
 
 gsize
 rspamd_strlcpy (gchar *dst, const gchar *src, gsize siz)
