1c7f982f9d7a3d49c16307c17507ed7aa964754f
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-05-26 09:20:29 +0100

    Update copyright.

--- src/libserver/events.c
+++ src/libserver/events.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2012, Vsevolod Stakhov
+ * Copyright (c) 2009-2015, Vsevolod Stakhov
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
