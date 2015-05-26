e57e401c728e565c15c239e54b2ad36b3c01c62c
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-05-26 17:37:45 +0100

    If replacement cannot be applied, just remove regexp.

--- src/plugins/lua/spamassassin.lua
+++ src/plugins/lua/spamassassin.lua
@@ -524,6 +524,9 @@ end
 
 local function sa_regexp_match(data, re, raw, rule)
   local res = 0
+  if not re then
+    return 0
+  end
   if rule['multiple'] then
     local lim = -1
     if rule['maxhits'] then
@@ -628,6 +631,7 @@ _.each(function(r)
       local nre = rspamd_regexp.create_cached(nexpr)
       if not nre then
         rspamd_logger.errx('cannot apply replacement for rule %1', r)
+        rule['re'] = nil
       else
         rspamd_logger.debugx('replace %1 -> %2', r, nexpr)
         rule['re'] = nre
