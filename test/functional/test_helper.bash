98a46bf694fba3a9130d5d6ce4a2399083423a15
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-07-02 19:08:01 +0100

    Store rspamd log permanently for functional tests.

--- test/functional/test_helper.bash
+++ test/functional/test_helper.bash
@@ -3,6 +3,7 @@ function run_rspamd() {
 	RSPAMD_GROUP=${RSPAMD_GROUP:-"nogroup"}
 	RSPAMD=${RSPAMD:-"$BATS_TEST_DIRNAME/../../src/rspamd"}
 	
+	if [ -f /tmp/rspamd-bats.log ] ; then rm -f /tmp/rspamd-bats.log ; fi
 	${RSPAMD} -c ${RSPAMD_CONFIG} -u ${RSPAMD_USER} -g ${RSPAMD_GROUP}
 }
 
