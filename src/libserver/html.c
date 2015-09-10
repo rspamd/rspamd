0affe52da0ca281a482f8dfeb0868529425f0377
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-09-10 13:47:28 +0100

    Be more carefull about newlines in html.

--- src/libserver/html.c
+++ src/libserver/html.c
@@ -1887,10 +1887,14 @@ rspamd_html_process_part_full (rspamd_mempool_t *pool, struct html_content *hc,
 					setbit (hc->tags_seen, cur_tag->id);
 				}
 
-				if ((cur_tag->id == Tag_P || cur_tag->id == Tag_BR ||
-						cur_tag->id == Tag_HR) && balanced) {
+				if ((cur_tag->flags & (FL_CLOSED|FL_CLOSING)) &&
+						(cur_tag->id == Tag_P || cur_tag->id == Tag_BR ||
+						cur_tag->id == Tag_HR || cur_tag->id == Tag_TR ||
+						cur_tag->id == Tag_DIV) && balanced) {
 					/* Insert newline */
-					g_byte_array_append (dest, "\r\n", 2);
+					if (dest->data[dest->len - 1] != '\n') {
+						g_byte_array_append (dest, "\r\n", 2);
+					}
 					save_space = FALSE;
 				}
 
