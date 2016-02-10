4857227f111446954da3b202d7318feb6fba705c
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-02-10 23:02:16 +0000

    Fix arguments order

--- src/libcryptobox/cryptobox.c
+++ src/libcryptobox/cryptobox.c
@@ -1105,7 +1105,7 @@ rspamd_cryptobox_encryptv_nm_inplace (struct rspamd_cryptobox_segment *segments,
 
 	rspamd_cryptobox_flush_outbuf (start_seg, outbuf, sizeof (outbuf) - remain,
 			seg_offset);
-	rspamd_cryptobox_cleanup (auth_ctx, enc_ctx, mode);
+	rspamd_cryptobox_cleanup (enc_ctx, auth_ctx, mode);
 }
 
 gboolean
