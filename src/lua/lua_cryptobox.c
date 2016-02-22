cc8c507ff084a0614c5b40cb68267670dbd49286
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-02-22 13:22:12 +0000

    Fix doc comments

--- src/lua/lua_cryptobox.c
+++ src/lua/lua_cryptobox.c
@@ -14,25 +14,16 @@
  * limitations under the License.
  */
 
-/*-
- * Copyright 2016 Vsevolod Stakhov
+/***
+ * @module rspamd_cryptobox
+ * Rspamd cryptobox is a module that operates with digital signatures and
+ * hashes.
+ * @example
+ * local hash = require "rspamd_cryptobox_hash"
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * @file lua_cryptobox.c
- * This module exports routines to load cryptobox keys, check inline or external
- * crypto signatures or encrypting/decrypting data.
+ * local h = hash.create()
+ * h:update('hello world')
+ * print(h:hex())
  */
 
 #include "lua_common.h"
