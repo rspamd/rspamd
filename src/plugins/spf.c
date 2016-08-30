bd8d0a556a8e864a1318e1e113a02a3f88f73dcd
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-08-30 12:49:02 +0100

    [Fix] Fix options for SPF dnsfail symbol

--- src/plugins/spf.c
+++ src/plugins/spf.c
@@ -501,12 +501,16 @@ spf_symbol_callback (struct rspamd_task *task, void *unused)
 		else {
 			w = rspamd_session_get_watcher (task->s);
 			if (!rspamd_spf_resolve (task, spf_plugin_callback, w)) {
+				GList *opts = NULL;
+
+				opts = g_list_prepend (opts, "(SPF): spf DNS fail");
 				msg_info_task ("cannot make spf request for [%s]",
 						task->message_id);
 				rspamd_task_insert_result (task,
 						spf_module_ctx->symbol_dnsfail,
 						1,
-						"(SPF): spf DNS fail");
+						opts);
+				g_list_free (opts);
 			}
 			else {
 				rspamd_session_watcher_push (task->s);
