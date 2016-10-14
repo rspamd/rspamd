734f2591003d9873bb614d10da6a120caf9c1560
Vsevolod Stakhov <vsevolod@highsecure.ru>
2016-10-14 11:40:11 +0100

    [Minor] Fix stupid mistakes in string compare functions

--- src/lua/lua_util.c
+++ src/lua/lua_util.c
@@ -1249,7 +1249,7 @@ lua_util_strcasecmp_utf8 (lua_State *L)
 	gint ret = -1;
 
 	str1 = lua_tolstring (L, 1, &len1);
-	str2 = lua_tolstring (L, 1, &len2);
+	str2 = lua_tolstring (L, 2, &len2);
 
 	if (str1 && str2) {
 
@@ -1276,7 +1276,7 @@ lua_util_strcasecmp_ascii (lua_State *L)
 	gint ret = -1;
 
 	str1 = lua_tolstring (L, 1, &len1);
-	str2 = lua_tolstring (L, 1, &len2);
+	str2 = lua_tolstring (L, 2, &len2);
 
 	if (str1 && str2) {
 
