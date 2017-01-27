63566714e3fe1ab35f2caf6c76e2f61af49d56c3
Vsevolod Stakhov <vsevolod@highsecure.ru>
2017-01-27 15:18:38 +0000

    [Fix] Add packed attribute for protocol structure

--- src/libserver/fuzzy_wire.h
+++ src/libserver/fuzzy_wire.h
@@ -84,8 +84,8 @@ struct rspamd_fuzzy_stat_entry {
 	guint32 fuzzy_cnt;
 };
 
-struct fuzzy_peer_cmd {
-	gboolean is_shingle;
+RSPAMD_PACKED(fuzzy_peer_cmd) {
+	gint32 is_shingle;
 	union {
 		struct rspamd_fuzzy_cmd normal;
 		struct rspamd_fuzzy_shingle_cmd shingle;
