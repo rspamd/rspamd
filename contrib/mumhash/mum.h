c38454330c20d13e8b8b1f05199c3a2545e20627
Vsevolod Stakhov <vsevolod@highsecure.ru>
2017-03-19 18:36:35 +0000

    [Minor] Fix aarch64 build

--- contrib/mumhash/mum.h
+++ contrib/mumhash/mum.h
@@ -114,7 +114,7 @@ _mum (uint64_t v, uint64_t p) {
      function doing 128x128->128 bit multiplication.  The function is
      very slow.  */
   lo = v * p, hi;
-  asm ("umulh %0, %1, %2" : "=r" (hi) : "r" (v), "r" (p));
+  __asm__ ("umulh %0, %1, %2" : "=r" (hi) : "r" (v), "r" (p));
 #else
   __uint128_t r = (__uint128_t) v * (__uint128_t) p;
   hi = (uint64_t) (r >> 64);
