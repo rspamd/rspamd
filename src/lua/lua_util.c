735f159a64d302681f8a836ad538ed399bbf7b28
Vsevolod Stakhov <vsevolod@highsecure.ru>
2017-05-11 19:03:21 +0100

    [Fix] Disable certain checks for utf spoof detection

--- src/lua/lua_util.c
+++ src/lua/lua_util.c
@@ -1951,6 +1951,9 @@ lua_util_is_utf_spoofed (lua_State *L)
 				return 1;
 			}
 
+			uspoof_setChecks (spc_sgl,
+					USPOOF_ALL_CHECKS & ~USPOOF_WHOLE_SCRIPT_CONFUSABLE,
+					&uc_err);
 			uspoof_setAllowedChars (spc_sgl, allowed, &uc_err);
 #if U_ICU_VERSION_MAJOR_NUM >= 51
 			uspoof_setRestrictionLevel (spc_sgl, USPOOF_MODERATELY_RESTRICTIVE);
