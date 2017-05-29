3360e946cec1582a50227983c623055e0b863e4e
Andrew Lewis <nerf@judo.za.org>
2017-05-29 11:17:41 +0200

    [Minor] Fix warnings

--- src/plugins/lua/dmarc.lua
+++ src/plugins/lua/dmarc.lua
@@ -608,7 +608,7 @@ if opts['reporting'] == true then
     end
     rspamd_config:add_on_load(function(cfg, ev_base, worker)
       if not (worker:get_name() == 'normal' and worker:get_index() == 0) then return end
-      local resolver = rspamd_resolver.init(ev_base, rspamd_config)
+      local rresolver = rspamd_resolver.init(ev_base, rspamd_config)
       load_scripts(cfg, ev_base)
       rspamd_config:register_finish_script(function ()
         local stamp = pool:get_variable(VAR_NAME, 'double')
@@ -856,7 +856,7 @@ if opts['reporting'] == true then
           stop_pattern = '\r\n',
           host = report_settings.smtp,
           port = report_settings.smtp_port,
-          resolver = resolver,
+          resolver = rresolver,
         })
       end
       local function make_report()
@@ -961,7 +961,7 @@ if opts['reporting'] == true then
                 reporting_addr[test_addr] = true
               end
             end
-            local t, vdom = next(to_verify)
+            local t, nvdom = next(to_verify)
             if not t then
               if next(reporting_addr) then
                 make_report()
@@ -970,7 +970,7 @@ if opts['reporting'] == true then
                 delete_reports()
               end
             else
-              verifier(t, vdom)
+              verifier(t, nvdom)
             end
           end
           rspamd_config:get_resolver():resolve_txt(nil, pool,
