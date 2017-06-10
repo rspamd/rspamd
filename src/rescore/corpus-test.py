e76bd1f6e8b6223a0cdb10d0c5178f9991388d55
Pragadeesh C <cpragadeesh@gmail.com>
2017-06-10 14:03:16 -0700

    multi-threaded corpus-testing

--- src/rescore/corpus-test.py
+++ src/rescore/corpus-test.py
@@ -8,6 +8,14 @@ import json
 from os.path import basename
 from requests.exceptions import RequestException
 from itertools import chain
+from Queue import Queue
+from threading import Thread
+
+
+test_queue = Queue()    # Each element is a tuple: (file_location, email_type)
+                        # email_type is either "SPAM" or "HAM"
+test_results = []
+
 
 def get_all_filenames(location):
     ''' Recursively gets a list of  all file names'''
@@ -40,23 +48,24 @@ def is_symbol_field(value):
     return is_symbol
 
 
-def test_files_from_dir(test_url, location, email_type):
+def get_symbols_from_response(metrics):
+                
+    symbols = []
+            
+    for field in metrics:
+        if is_symbol_field(metrics[field]):
+            symbols.append(field)
 
-    if email_type not in ['HAM', 'SPAM']:
-        # TODO : Handle exception
-        raise ValueError("Wrong email_type")
-        return
-    
-    if not os.path.isdir(location):
-        print "{} is does not exists or is not a directory.".format(location)
-        return
+    return symbols
 
-    filenames = get_all_filenames(location) 
+            
+def test_email(test_url):
 
-    test_results = []
+    while True:
 
-    for filename in filenames:
-        with open(filename, 'r') as f:
+        file_location, email_type = test_queue.get()
+
+        with open(file_location, 'r') as f:
             try:
                 response = requests.post(test_url, data=f)
             except RequestException as err:
@@ -65,33 +74,65 @@ def test_files_from_dir(test_url, location, email_type):
             else:
                 metrics = json.loads(response.text)["default"]
 
-                symbols = []
-            
-                for field in metrics:
-                    if is_symbol_field(metrics[field]):
-                        symbols.append(field)
+                symbols = get_symbols_from_response(metrics)
 
-                test_results.append('{} {} {} {}'.format(basename(filename),
+                test_results.append('{} {} {} {}'.format(basename(file_location),
                                                          email_type,
                                                          metrics['score'],
                                                          ' '.join(symbols)))
+        test_queue.task_done()
 
-    return test_results
-                                    
-
-def write_test_results(results, filename):
-
-    with open(filename, 'a') as out:
-        out.write('\r\n'.join(results))
         
+def test_emails_from_dir(location, email_type):
+
+    if email_type not in ['HAM', 'SPAM']:
+        raise ValueError("Wrong email_type")
+        return
+
+    filenames = []
+    
+    if os.path.isdir(location):
+        filenames = get_all_filenames(location) 
+    else:
+        filenames.append(location)
+
+    for filename in filenames:
+        test_queue.put((filename, email_type))
+
+        
+def write_test_results(output_file, test_results):
+
+    with open(output_file, 'w') as out:
+        out.write('\r\n'.join(test_results))
+        
+
+def run_tests(test_url, ham_location, spam_location, no_of_threads=2):
+
+    threads = [Thread(target=test_email, args=(test_url, ))
+               for _ in range(no_of_threads)]
+
+    for thread in threads:
+        thread.setDaemon(True)
+        thread.start()
+
+    if ham_location:
+        test_emails_from_dir(ham_location, "HAM")
+    if spam_location:
+        test_emails_from_dir(spam_location, "SPAM")
+
+    test_queue.join()
+
+
+    return
+    
     
 def main():
 
     rspamd_host = "127.0.0.1"
     rspamd_port = 11333
-
     output_file = "results.log"
-    
+    no_of_threads = 2
+
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-a", "--ham", help="path to ham directory")
     arg_parser.add_argument("-s", "--spam", help="path to spam directory")
@@ -101,6 +142,9 @@ def main():
                             help="ip of rspamd service [Default: 127.0.0.1]")
     arg_parser.add_argument("-p", "--port",
                             help="rspamd service port number [Default: 11333]",
+                            type=int)
+    arg_parser.add_argument("-j", "--threads",
+                            help="number of threads to run tests",
                             type=int)
     
     args = arg_parser.parse_args()
@@ -118,22 +162,20 @@ def main():
 
     if args.output:
         output_file = args.output
-        
+
+    if args.threads:
+        no_of_threads = args.threads
+
+
     test_url = get_test_url(rspamd_host, rspamd_port)
 
-    ham_test_results = []
-    spam_test_results = []
+    run_tests(test_url=test_url,
+              ham_location=args.ham,
+              spam_location=args.spam,
+              no_of_threads=no_of_threads)
+
+    write_test_results(output_file, test_results)
+
     
-    if args.ham:
-        ham_test_results = test_files_from_dir(test_url, args.ham, 'HAM')
-
-    if args.spam:
-        spam_test_results = test_files_from_dir(test_url, args.spam, 'SPAM')
-
-    test_results = chain(ham_test_results, spam_test_results)
-
-    write_test_results(test_results, output_file)
-
-        
 if __name__ == "__main__":
     main()
