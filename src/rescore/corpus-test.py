ccf861bf8bf74b35afab1b1883d1538c117868da
Pragadeesh C <cpragadeesh@gmail.com>
2017-06-13 04:45:55 -0700

    improved corpus-test reporting

--- src/rescore/corpus-test.py
+++ src/rescore/corpus-test.py
@@ -10,10 +10,12 @@ from requests.exceptions import RequestException
 from itertools import chain
 from Queue import Queue
 from threading import Thread
-
+import sys
+import time
 
 test_queue = Queue()    # Each element is a tuple: (file_location, email_type)
                         # email_type is either "SPAM" or "HAM"
+progress_queue = Queue()                        
 test_results = []
 
 
@@ -58,6 +60,30 @@ def get_symbols_from_response(metrics):
 
     return symbols
 
+
+def progress_bar(total_emails, width=50):
+
+    done = 0
+
+    print '[{}]'.format(' ' * width),
+    
+    while True:
+        temp = progress_queue.get()
+        done += 1
+        complete_width = done * width / total_emails
+        
+        print '\r[{}{}] ({} / {}) done'.format('#' * complete_width,
+                                               ' ' * (width - complete_width),
+                                               done,
+                                               total_emails),
+
+        if done == total_emails:
+            print
+
+        sys.stdout.flush()
+        
+        progress_queue.task_done()
+            
             
 def test_email(test_url):
 
@@ -67,71 +93,106 @@ def test_email(test_url):
 
         with open(file_location, 'r') as f:
             try:
+                # TODO pass file /path instead                
                 response = requests.post(test_url, data=f)
             except RequestException as err:
                 print "Connection error."
                 print err
             else:
-                metrics = json.loads(response.text)["default"]
 
+                try:
+                    metrics = json.loads(response.text)["default"]
+                except KeyError:
+                    print "Error testing email: {}. Skipping.".format(file_location)
+                    continue
+                
                 symbols = get_symbols_from_response(metrics)
 
                 test_results.append('{} {} {} {}'.format(basename(file_location),
                                                          email_type,
                                                          metrics['score'],
                                                          ' '.join(symbols)))
+
+        progress_queue.put("DONE")
         test_queue.task_done()
 
         
-def test_emails_from_dir(location, email_type):
-
+def test_emails(filenames, email_type):
+    
     if email_type not in ['HAM', 'SPAM']:
         raise ValueError("Wrong email_type")
         return
 
-    filenames = []
-    
-    if os.path.isdir(location):
-        filenames = get_all_filenames(location) 
-    else:
-        filenames.append(location)
-
     for filename in filenames:
         test_queue.put((filename, email_type))
-
+    
         
 def write_test_results(output_file, test_results):
 
     with open(output_file, 'w') as out:
         out.write('\r\n'.join(test_results))
-        
 
+
+def print_test_stats(total_time, avg_time, no_of_emails):
+
+    print "Test stats: "
+    print "Total time: {}s".format(total_time)
+    print "Avg. time per email: {}s".format(avg_time)
+    print "Emails scanned: {}".format(no_of_emails)
+
+    
 def run_tests(test_url, ham_location, spam_location, no_of_threads=2):
 
+    start_time = time.time()
+    
+    ham_file_locations = []
+    spam_file_locations = []
+
+    if ham_location:
+        if os.path.isdir(ham_location):
+            ham_file_locations = get_all_filenames(ham_location)
+        else:
+            ham_file_locations.append(ham_location)
+
+    if spam_location:
+        if os.path.isdir(spam_location):
+            spam_file_locations = get_all_filenames(spam_location)
+        else:
+            spam_file_locations.append(spam_location)
+
+    no_of_emails = len(ham_file_locations) + len(spam_file_locations)
+    
     threads = [Thread(target=test_email, args=(test_url, ))
-               for _ in range(no_of_threads)]
+               for _ in range(no_of_threads)]        
+
+    progress_thread = Thread(target=progress_bar, args=(no_of_emails, ))
+    progress_thread.setDaemon(True)
+    progress_thread.start()
 
     for thread in threads:
         thread.setDaemon(True)
         thread.start()
 
-    if ham_location:
-        test_emails_from_dir(ham_location, "HAM")
-    if spam_location:
-        test_emails_from_dir(spam_location, "SPAM")
+    test_emails(ham_file_locations, "HAM")
+    test_emails(spam_file_locations, "SPAM")
 
     test_queue.join()
+    progress_queue.join()
+
+    total_time = round(time.time() - start_time, 2)
+    avg_time = round(total_time / no_of_emails, 2)
+
+    print_test_stats(total_time=total_time,
+                     avg_time=avg_time,
+                     no_of_emails=no_of_emails)
 
 
-    return
-    
-    
 def main():
 
     rspamd_host = "127.0.0.1"
     rspamd_port = 11333
     output_file = "results.log"
-    no_of_threads = 2
+    no_of_threads = 10
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-a", "--ham", help="path to ham directory")
@@ -144,7 +205,7 @@ def main():
                             help="rspamd service port number [Default: 11333]",
                             type=int)
     arg_parser.add_argument("-j", "--threads",
-                            help="number of threads to run tests",
+                            help="number of threads to run tests [Default: 10]",
                             type=int)
     
     args = arg_parser.parse_args()
@@ -168,7 +229,7 @@ def main():
 
 
     test_url = get_test_url(rspamd_host, rspamd_port)
-
+    
     run_tests(test_url=test_url,
               ham_location=args.ham,
               spam_location=args.spam,
