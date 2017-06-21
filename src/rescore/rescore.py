0621136a8fe60ed6cb36303e5f83aac2f55aec13
Pragadeesh C <cpragadeesh@gmail.com>
2017-06-21 04:32:51 -0700

    added dataset normalizer

--- src/rescore/rescore.py
+++ src/rescore/rescore.py
@@ -5,25 +5,49 @@ import os
 
 from utility import get_all_filenames
 
-def filter_logs(logdir):
-    '''Makes a list of lists. Each list contains symbols hit by an email'''
+def get_dataset_from_logs(logdir):
+    '''
+    Return X, y
+    X is a list of lists. Each list contains symbols hit by an email
+    y is a list of actual email class. Yi = 1 ith email is spam, 0 otherwise
+    '''
     
     filenames = get_all_filenames(logdir)
+
+    X = []
+    y = []
     
-    dataset = [open(file, 'r').read().split(' ')[4:] for file in filenames]
+    for file in filenames:
+        with open(file, 'r') as f:
+            for line in f:
+                X.append(line.split()[3:])
+                y.append(0 if line.split()[1] == "HAM" else 1)
 
-    return dataset
+    return X, y
 
 
-def get_symbol_set(dataset):
+def get_all_symbols(dataset):
 
     symbol_set = set()
 
-    for record in dataset:
-        map(lambda x : symbol_set.add(x), record)
+    for row in dataset:
+        map(lambda x : symbol_set.add(x), row)
 
-    return symbol_set
+    return tuple(symbol_set)
 
+def make_perceptron_input(X, symbol_set):
+    '''
+    Returns a list of lists containing 1s and 0s. 
+    X(i, j) = 1 if symbol_set(j) is present in ith log line, 0 otherwise
+    '''
+
+    X_new = []
+
+    for row in X:
+        X_new.append([1 if symbol in row else 0 for symbol in symbol_set])
+
+    return X_new
+    
 
 def main():
 
@@ -37,10 +61,13 @@ def main():
         arg_parser.error('specify log directory')
         return
         
-    dataset = filter_logs(logdir=args.logdir)
+    X, y = get_dataset_from_logs(logdir=args.logdir)
     
-    symbol_set = get_symbol_set(dataset)
+    symbol_set = get_all_symbols(X)
 
-    
+    X = make_perceptron_input(X=X,
+                              symbol_set=symbol_set)
+
+
 if __name__ == "__main__":
     main()
