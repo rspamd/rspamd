fa724a46030d58d6faf2215c5f544aaefc52ff4e
Andrew Lewis <nerf@judo.za.org>
2017-08-28 12:16:26 +0200

    [Test] Settings: few more tests

--- test/functional/lua/settings.lua
+++ test/functional/lua/settings.lua
@@ -2,6 +2,7 @@ rspamd_config:register_symbol({
   name = 'SIMPLE_PRE',
   score = 1.0,
   priority = 9, -- after settings
+  group = 'a',
   type = 'prefilter',
   callback = function()
     return true, 'Fires always'
@@ -12,6 +13,7 @@ rspamd_config:register_symbol({
   name = 'SIMPLE_POST',
   score = 1.0,
   type = 'postfilter',
+  group = 'c',
   callback = function()
     return true, 'Fires always'
   end
@@ -20,6 +22,7 @@ rspamd_config:register_symbol({
 rspamd_config:register_symbol({
   name = 'SIMPLE_TEST',
   score = 1.0,
+  group = 'b',
   callback = function()
     return true, 'Fires always'
   end
