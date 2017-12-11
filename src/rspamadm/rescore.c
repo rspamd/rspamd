632a95456f62eb996e59c3a9515da9f616473ed2
Vsevolod Stakhov <vsevolod@highsecure.ru>
2017-12-11 07:24:55 +0000

    [Minor] Reformat and fix minor style issues in rescore

--- src/rspamadm/rescore.c
+++ src/rspamadm/rescore.c
@@ -14,128 +14,126 @@
  * limitations under the License.
  */
 
-#include "rspamadm.h"
 #include "config.h"
+#include "rspamadm.h"
 #include "lua/lua_common.h"
 
 #if !defined(WITH_TORCH) || !defined(WITH_LUAJIT)
-    #define HAS_TORCH false
+#define HAS_TORCH false
 #else
-    #define HAS_TORCH true
+#define HAS_TORCH true
 #endif
 
-static gchar *logdir = NULL; 
+static gchar *logdir = NULL;
 static gchar *output = "new.scores";
-static gdouble threshold = 15; // Spam threshold
-static gboolean score_diff = false;  // Print score diff flag
-static gint64 iters = 500; // Perceptron max iterations
+static gdouble threshold = 15; /* Spam threshold */
+static gboolean score_diff = false;  /* Print score diff flag */
+static gint64 iters = 500; /* Perceptron max iterations */
+
+/* TODO: think about adding the config file reading */
 
 static void rspamadm_rescore (gint argc, gchar **argv);
+
 static const char *rspamadm_rescore_help (gboolean full_help);
 
 struct rspamadm_command rescore_command = {
-    .name = "rescore",
-    .flags = 0,
-    .help = rspamadm_rescore_help,
-    .run = rspamadm_rescore
+		.name = "rescore",
+		.flags = 0,
+		.help = rspamadm_rescore_help,
+		.run = rspamadm_rescore
 };
 
 static GOptionEntry entries[] = {
-            {"logdir", 'l', 0, G_OPTION_ARG_FILENAME, &logdir,
-                                        "Logs directory", NULL},
-            {"output", 'o', 0, G_OPTION_ARG_FILENAME, &output,
-                                    "Scores output locaiton", NULL},
-            {"diff", 'd', 0, G_OPTION_ARG_NONE, &score_diff,
-                                    "Print score diff", NULL},
-            {"iters", 'i', 0, G_OPTION_ARG_INT64, &iters,
-                "Max iterations for perceptron [Default: 500]", NULL},  
-            {NULL,  0,  0, G_OPTION_ARG_NONE, NULL, NULL, NULL}
+		{"logdir", 'l', 0, G_OPTION_ARG_FILENAME, &logdir,
+				"Logs directory",                               NULL},
+		{"output", 'o', 0, G_OPTION_ARG_FILENAME, &output,
+				"Scores output locaiton",                       NULL},
+		{"diff",   'd', 0, G_OPTION_ARG_NONE,     &score_diff,
+				"Print score diff",                             NULL},
+		{"iters",  'i', 0, G_OPTION_ARG_INT64,    &iters,
+				"Max iterations for perceptron [Default: 500]", NULL},
+		{NULL,     0,   0, G_OPTION_ARG_NONE, NULL, NULL,       NULL}
 };
 
 static const char *
-rspamadm_rescore_help (gboolean full_help)
-{
+rspamadm_rescore_help (gboolean full_help) {
 
-    const char *help_str;
+	const char *help_str;
 
-    if (full_help) {
-        help_str = "Estimate optimal symbol weights from log files\n\n"
-                    "Usage: rspamadm rescore -l <log_directory>\n"
-                    "Where options are:\n\n"
-                    "-l: path to logs directory\n"
-                    "-o: Scores output file location\n"
-                    "-d: Print scores diff\n"
-                    "-i: Max iterations for perceptron\n";
-    }
+	if (full_help) {
+		help_str = "Estimate optimal symbol weights from log files\n\n"
+				"Usage: rspamadm rescore -l <log_directory>\n"
+				"Where options are:\n\n"
+				"-l: path to logs directory\n"
+				"-o: Scores output file location\n"
+				"-d: Print scores diff\n"
+				"-i: Max iterations for perceptron\n";
+	} else {
+		help_str = "Estimate optimal symbol weights from log files";
+	}
 
-    else {
-        help_str = "Estimate optimal symbol weights from log files";
-    }
-
-    return help_str;
+	return help_str;
 }
 
-static void 
-rspamadm_rescore (gint argc, gchar **argv)
-{
+static void
+rspamadm_rescore (gint argc, gchar **argv) {
 
-    GOptionContext *context;
-    GError *error = NULL;
-    lua_State *L;
-    ucl_object_t *obj;
+	GOptionContext *context;
+	GError *error = NULL;
+	lua_State *L;
+	ucl_object_t *obj;
 
-    context = g_option_context_new (
-        "rescore - Estimate optimal symbol weights from log files");
+	context = g_option_context_new (
+			"rescore - Estimate optimal symbol weights from log files");
 
-    g_option_context_set_summary (context,
-        "Summary:\n Rspamd administration utility version "
-                        RVERSION
-                        "\n Release id: "
-                        RID);
+	g_option_context_set_summary (context,
+			"Summary:\n Rspamd administration utility version "
+					RVERSION
+					"\n Release id: "
+					RID);
 
-    g_option_context_add_main_entries (context, entries, NULL);
-    g_option_context_set_ignore_unknown_options (context, TRUE);
+	g_option_context_add_main_entries (context, entries, NULL);
+	g_option_context_set_ignore_unknown_options (context, TRUE);
 
-    if (!g_option_context_parse (context, &argc, &argv, &error)) {
-        rspamd_fprintf (stderr, "option parsing failed: %s\n", error->message);
-        g_error_free (error);
-        exit(1);
-    }
+	if (!g_option_context_parse (context, &argc, &argv, &error)) {
+		rspamd_fprintf (stderr, "option parsing failed: %s\n", error->message);
+		g_error_free (error);
+		exit (EXIT_FAILURE);
+	}
 
-    if (!HAS_TORCH) {
-        rspamd_fprintf (stderr, "Torch is not enabled. "
-            "Use -DENABLE_TORCH=ON option while running cmake.\n");
-        exit (1);
-    }
+	if (!HAS_TORCH) {
+		rspamd_fprintf (stderr, "Torch is not enabled. "
+				"Use -DENABLE_TORCH=ON option while running cmake.\n");
+		exit (EXIT_FAILURE);
+	}
 
-    if (logdir == NULL) {
-        rspamd_fprintf (stderr, "Please specify log directory.\n");
-        exit (1);
-    }
+	if (logdir == NULL) {
+		rspamd_fprintf (stderr, "Please specify log directory.\n");
+		exit (EXIT_FAILURE);
+	}
 
-    L = rspamd_lua_init ();
+	L = rspamd_lua_init ();
+	rspamd_lua_set_path (L, NULL, NULL);
 
-    rspamd_lua_set_path(L, NULL, NULL);
+	obj = ucl_object_typed_new (UCL_OBJECT);
 
-    obj = ucl_object_typed_new (UCL_OBJECT);
+	ucl_object_insert_key (obj, ucl_object_fromstring (logdir),
+			"logdir", 0, false);
+	ucl_object_insert_key (obj, ucl_object_fromstring (output),
+			"output", 0, false);
+	ucl_object_insert_key (obj, ucl_object_fromdouble (threshold),
+			"threshold", 0, false);
+	ucl_object_insert_key (obj, ucl_object_fromint (iters),
+			"iters", 0, false);
+	ucl_object_insert_key (obj, ucl_object_frombool (score_diff),
+			"diff", 0, false);
 
-    ucl_object_insert_key (obj, ucl_object_fromstring (logdir),
-                                            "logdir", 0, false);
-    ucl_object_insert_key (obj, ucl_object_fromstring (output),
-                                            "output", 0, false);
-    ucl_object_insert_key (obj, ucl_object_fromdouble (threshold),
-                                            "threshold", 0, false);
-    ucl_object_insert_key (obj, ucl_object_fromint (iters),
-                                            "iters", 0, false);
-    ucl_object_insert_key (obj, ucl_object_frombool (score_diff),
-                                            "diff", 0, false);
+	rspamadm_execute_lua_ucl_subr (L,
+			argc,
+			argv,
+			obj,
+			"rescore");
 
-    rspamadm_execute_lua_ucl_subr (L,
-                        argc,
-                        argv,
-                        obj,
-                        "rescore");
-
-    lua_close (L);
-    ucl_object_unref (obj);
+	lua_close (L);
+	ucl_object_unref (obj);
 }
