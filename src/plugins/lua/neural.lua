6277eb78349c2c364fb99396efe0d03c1bc8656c
Vsevolod Stakhov <vsevolod@highsecure.ru>
2018-04-05 18:47:07 +0100

    [Minor] Remove unused function

--- src/plugins/lua/neural.lua
+++ src/plugins/lua/neural.lua
@@ -960,21 +960,6 @@ else
     priority = 6,
     callback = ann_scores_filter
   })
-
-  local function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-      copy = {}
-      for orig_key, orig_value in next, orig, nil do
-        copy[deepcopy(orig_key)] = deepcopy(orig_value)
-      end
-      setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-      copy = orig
-    end
-    return copy
-  end
   for k,r in pairs(rules) do
     local def_rules = lua_util.override_defaults(default_options, r)
     def_rules['redis'] = redis_params
