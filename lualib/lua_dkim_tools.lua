474a85f597791f1ed885b8b6ed9b0419b3bec46e
bjoe2k4 <bjoe2k4@users.noreply.github.com>
2018-04-16 20:56:33 +0200

    [Fix] Typo use_domain_local --> use_domain_sign_local
    use_domain_sign_local is documented on rspamd.com (https://rspamd.com/doc/modules/dkim_signing.html)

--- lualib/lua_dkim_tools.lua
+++ lualib/lua_dkim_tools.lua
@@ -94,10 +94,10 @@ local function prepare_dkim_signing(N, task, settings)
     dkim_domain = get_dkim_domain('use_domain_sign_networks')
     rspamd_logger.debugm(N, task, 'sign_networks: use domain(%s) for signature: %s',
       settings.use_domain_sign_networks, dkim_domain)
-  elseif settings.use_domain_local and is_local then
-    dkim_domain = get_dkim_domain('use_domain_local')
+  elseif settings.use_domain_sign_local and is_local then
+    dkim_domain = get_dkim_domain('use_domain_sign_local')
     rspamd_logger.debugm(N, task, 'local: use domain(%s) for signature: %s',
-      settings.use_domain_local, dkim_domain)
+      settings.use_domain_sign_local, dkim_domain)
   else
     dkim_domain = get_dkim_domain('use_domain')
     rspamd_logger.debugm(N, task, 'use domain(%s) for signature: %s',
