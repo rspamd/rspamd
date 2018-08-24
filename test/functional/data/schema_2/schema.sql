213ba86aaa6c1cf62210db1ce19213eeb6f6d5de
Mikhail Galanin <mgalanin@mimecast.com>
2018-08-24 09:20:06 +0100

    [Test] More test cases for ClickHouse

---
+++ test/functional/data/schema_2/schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE rspamd
+(
+    Date Date,
+    TS DateTime,
+    From String,
+    MimeFrom String,
+    IP String,
+    Score Float64,
+    NRcpt UInt8,
+    Size UInt32,
+    IsWhitelist Enum8('blacklist' = 0, 'whitelist' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('blacklist' = 0, 'whitelist' = 1, 'unknown' = 2)),
+    IsBayes Enum8('ham' = 0, 'spam' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('ham' = 0, 'spam' = 1, 'unknown' = 2)),
+    IsFuzzy Enum8('whitelist' = 0, 'deny' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('whitelist' = 0, 'deny' = 1, 'unknown' = 2)),
+    IsFann Enum8('ham' = 0, 'spam' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('ham' = 0, 'spam' = 1, 'unknown' = 2)),
+    IsDkim Enum8('reject' = 0, 'allow' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('reject' = 0, 'allow' = 1, 'unknown' = 2)),
+    IsDmarc Enum8('reject' = 0, 'allow' = 1, 'unknown' = 2) DEFAULT CAST('unknown' AS Enum8('reject' = 0, 'allow' = 1, 'unknown' = 2)),
+    NUrls Int32,
+    Action Enum8('reject' = 0, 'rewrite subject' = 1, 'add header' = 2, 'greylist' = 3, 'no action' = 4, 'soft reject' = 5) DEFAULT CAST('no action' AS Enum8('reject' = 0, 'rewrite subject' = 1, 'add header' = 2, 'greylist' = 3, 'no action' = 4, 'soft reject' = 5)),
+    FromUser String,
+    MimeUser String,
+    RcptUser String,
+    RcptDomain String,
+    ListId String,
+    `Attachments.FileName` Array(String),
+    `Attachments.ContentType` Array(String),
+    `Attachments.Length` Array(UInt32),
+    `Attachments.Digest` Array(FixedString(16)),
+    `Urls.Tld` Array(String),
+    `Urls.Url` Array(String),
+    Emails Array(String),
+    ASN String,
+    Country FixedString(2),
+    IPNet String,
+    `Symbols.Names` Array(String),
+    `Symbols.Scores` Array(Float64),
+    `Symbols.Options` Array(String),
+    Digest FixedString(32)
+) ENGINE = MergeTree(Date, (TS, From), 8192);
+
+CREATE TABLE rspamd_version ( Version UInt32) ENGINE = TinyLog;
+
+INSERT INTO rspamd_version (Version) Values (2);
