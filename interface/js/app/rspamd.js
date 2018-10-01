8913c7a9c31f20ab7ba6ae16b042a0a0c0ed879c
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-10-01 21:15:55 +0200

    Move code to ui.connect

--- interface/js/app/rspamd.js
+++ interface/js/app/rspamd.js
@@ -274,19 +274,6 @@ function ($, D3pie, visibility, NProgress, tab_stat, tab_graph, tab_config,
             }, 1000);
         });
 
-        $.ajax({
-                type: 'GET',
-                url: 'stat',
-                success: function () {
-                    saveCredentials({}, 'nopassword');
-                    var dialog = $('#connectDialog');
-                    var backdrop = $('#backDrop');
-                    $(dialog).hide();
-                    $(backdrop).hide();
-                    displayUI();
-                },
-        });
-
         $("a[data-toggle=\"tab\"]").on("click", function (e) {
             var tab_id = "#" + $(e.target).attr("id");
             tabClick(tab_id);
@@ -313,6 +300,18 @@ function ($, D3pie, visibility, NProgress, tab_stat, tab_graph, tab_config,
     };
 
     ui.connect = function () {
+
+        $.ajax({
+                type: 'GET',
+                url: 'stat',
+                success: function () {
+                    saveCredentials({}, 'nopassword');
+                    $(dialog).hide();
+                    $(backdrop).hide();
+                    displayUI();
+                },
+        });
+
         if (isLogged()) {
             displayUI();
             return;
