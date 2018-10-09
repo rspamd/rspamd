a445d8920d17bc3c65b7d02fbcf7cb82e656eca3
Mikhail Galanin <mgalanin@mimecast.com>
2018-10-09 15:58:28 +0100

    [Minor] Changed default paths

--- test/functional/util/merge_coveralls.py
+++ test/functional/util/merge_coveralls.py
@@ -31,9 +31,9 @@ path_mapping = [
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--input', type=open, required=True, nargs='+', help='input files')
 parser.add_argument('--output', type=str, required=True, help='output file)')
-parser.add_argument('--root', type=str, required=False, default="/home/circleci/project", help='repository root)')
-parser.add_argument('--install-dir', type=str, required=False, default="/home/circleci/install", help='install root)')
-parser.add_argument('--build-dir', type=str, required=False, default="/home/circleci/build", help='build root)')
+parser.add_argument('--root', type=str, required=False, default="/rspamd/src/github.com/rspamd/rspamd", help='repository root)')
+parser.add_argument('--install-dir', type=str, required=False, default="/rspamd/install", help='install root)')
+parser.add_argument('--build-dir', type=str, required=False, default="/rspamd/build", help='build root)')
 parser.add_argument('--token', type=str, help='If present, the file will be uploaded to coveralls)')
 
 def merge_coverage_vectors(c1, c2):
