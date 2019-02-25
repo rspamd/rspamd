ab43e080ebc5fea5a2c54bcad9180202b1a38711
Vsevolod Stakhov <vsevolod@highsecure.ru>
2019-02-25 18:19:51 +0000

    [Feature] Try to filter bad unicode types during normalisation

--- src/libstat/stat_api.h
+++ src/libstat/stat_api.h
@@ -39,6 +39,7 @@
 #define RSPAMD_STAT_TOKEN_FLAG_STOP_WORD (1u << 10)
 #define RSPAMD_STAT_TOKEN_FLAG_SKIPPED (1u << 11)
 #define RSPAMD_STAT_TOKEN_FLAG_INVISIBLE_SPACES (1u << 12)
+#define RSPAMD_STAT_TOKEN_FLAG_EMOJI (1u << 13)
 
 typedef struct rspamd_stat_token_s {
 	rspamd_ftok_t original; /* utf8 raw */
