b5f639d991c4cbeca0328578bf41ca957a03e596
Vsevolod Stakhov <vsevolod@highsecure.ru>
2019-07-21 09:38:30 +0100

    [Minor] Tell the exact task timeout value

--- src/worker.c
+++ src/worker.c
@@ -141,7 +141,8 @@ rspamd_task_timeout (EV_P_ ev_timer *w, int revents)
 	struct rspamd_task *task = (struct rspamd_task *)w->data;
 
 	if (!(task->processed_stages & RSPAMD_TASK_STAGE_FILTERS)) {
-		msg_info_task ("processing of task timed out, forced processing");
+		msg_info_task ("processing of task time out: %.1f second spent; forced processing",
+				ev_now (task->event_loop) - task->task_timestamp);
 
 		if (task->cfg->soft_reject_on_timeout) {
 			struct rspamd_action *action, *soft_reject;
