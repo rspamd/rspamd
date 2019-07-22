c36131d2bd53707f71ad9c63875db5d506f3b45f
Vsevolod Stakhov <vsevolod@highsecure.ru>
2019-07-22 16:10:20 +0100

    [Fix] Fix eviction corner case
    Issue: #2991

--- src/libutil/hash.c
+++ src/libutil/hash.c
@@ -481,6 +481,10 @@ rspamd_lru_hash_evict (rspamd_lru_hash_t *hash, time_t now)
 				}
 			}
 		});
+
+		if (selected) {
+			elt = selected;
+		}
 	}
 	else {
 		/* Fast random eviction */
@@ -493,6 +497,7 @@ rspamd_lru_hash_evict (rspamd_lru_hash_t *hash, time_t now)
 		}
 	}
 
+	/* Evict if nothing else has been cleaned */
 	if (elt && nexpired == 0) {
 		rspamd_lru_hash_remove_node (hash, elt);
 	}
