e25050699a85e1d34da566ea225f97c0069220c2
Carsten Rosenberg <c.rosenberg@heinlein-support.de>
2019-10-17 18:47:57 +0200

    [Minor] init razor plugin

--- lualib/lua_scanners/init.lua
+++ lualib/lua_scanners/init.lua
@@ -45,6 +45,7 @@ require_scanner('icap')
 require_scanner('vadesecure')
 require_scanner('spamassassin')
 require_scanner('p0f')
+require_scanner('razor')
 
 exports.add_scanner = function(name, t, conf_func, check_func)
   assert(type(conf_func) == 'function' and type(check_func) == 'function',
