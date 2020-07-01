b4fc5edd3710c2a8b896497f29077c55f558d318
Vsevolod Stakhov <vsevolod@highsecure.ru>
2020-07-01 13:24:01 +0100

    [Minor] Attach ical urls to the mime part

--- lualib/lua_content/ical.lua
+++ lualib/lua_content/ical.lua
@@ -42,7 +42,7 @@ local function extract_text_data(specific)
   return table.concat(tbl, '\n')
 end
 
-local function process_ical(input, _, task)
+local function process_ical(input, mpart, task)
   local control={n='\n', r=''}
   local rspamd_url = require "rspamd_url"
   local escaper = l.Ct((gen_grammar() / function(key, value)
@@ -54,7 +54,7 @@ local function process_ical(input, _, task)
       for _,u in ipairs(local_urls) do
         lua_util.debugm(N, task, 'ical: found URL in ical %s',
             tostring(u))
-        task:inject_url(u)
+        task:inject_url(u, mpart)
       end
     end
     lua_util.debugm(N, task, 'ical: ical key %s = "%s"',
