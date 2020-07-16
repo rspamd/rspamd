19eaa2c240065900fad96c842bee85969f06b564
Vsevolod Stakhov <vsevolod@highsecure.ru>
2020-07-16 21:24:55 +0100

    [Fix] Exclude damaged urls from html parser

--- src/libserver/html.c
+++ src/libserver/html.c
@@ -1518,7 +1518,7 @@ rspamd_html_process_url (rspamd_mempool_t *pool, const gchar *start, guint len,
 
 	/* Filter some completely damaged urls */
 	if (rc == URI_ERRNO_OK && url->hostlen > 0 &&
-		!((url->flags & RSPAMD_URL_FLAG_OBSCURED) && (url->protocol & PROTOCOL_UNKNOWN))) {
+		!((url->protocol & PROTOCOL_UNKNOWN))) {
 		url->flags |= saved_flags;
 
 		if (has_bad_chars) {
