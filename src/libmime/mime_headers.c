024a4e2fbcd665cdd29be54e7507b1bbdb0fbbea
Vsevolod Stakhov <vsevolod@highsecure.ru>
2020-08-05 10:16:15 +0100

    [Fix] Another try to fix email names parsing
    Related to: 772964f83b82f6d597e22c8b4d08220ab7df3f43#diff-677ba97322f6447774f021d51913b00b

--- src/libmime/mime_headers.c
+++ src/libmime/mime_headers.c
@@ -67,25 +67,25 @@ rspamd_mime_header_check_special (struct rspamd_task *task,
 		break;
 	case 0x76F31A09F4352521ULL:	/* to */
 		MESSAGE_FIELD (task, rcpt_mime) = rspamd_email_address_from_mime (task->task_pool,
-				rh->decoded, strlen (rh->decoded),
+				rh->value, strlen (rh->value),
 				MESSAGE_FIELD (task, rcpt_mime), max_recipients);
 		rh->flags |= RSPAMD_HEADER_TO|RSPAMD_HEADER_RCPT|RSPAMD_HEADER_UNIQUE;
 		break;
 	case 0x7EB117C1480B76ULL:	/* cc */
 		MESSAGE_FIELD (task, rcpt_mime) = rspamd_email_address_from_mime (task->task_pool,
-				rh->decoded, strlen (rh->decoded),
+				rh->value, strlen (rh->value),
 				MESSAGE_FIELD (task, rcpt_mime), max_recipients);
 		rh->flags |= RSPAMD_HEADER_CC|RSPAMD_HEADER_RCPT|RSPAMD_HEADER_UNIQUE;
 		break;
 	case 0xE4923E11C4989C8DULL:	/* bcc */
 		MESSAGE_FIELD (task, rcpt_mime) = rspamd_email_address_from_mime (task->task_pool,
-				rh->decoded, strlen (rh->decoded),
+				rh->value, strlen (rh->value),
 				MESSAGE_FIELD (task, rcpt_mime), max_recipients);
 		rh->flags |= RSPAMD_HEADER_BCC|RSPAMD_HEADER_RCPT|RSPAMD_HEADER_UNIQUE;
 		break;
 	case 0x41E1985EDC1CBDE4ULL:	/* from */
 		MESSAGE_FIELD (task, from_mime) = rspamd_email_address_from_mime (task->task_pool,
-				rh->decoded, strlen (rh->decoded),
+				rh->value, strlen (rh->value),
 				MESSAGE_FIELD (task, from_mime), max_recipients);
 		rh->flags |= RSPAMD_HEADER_FROM|RSPAMD_HEADER_SENDER|RSPAMD_HEADER_UNIQUE;
 		break;
