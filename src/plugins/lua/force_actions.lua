86364a68a6135d22f2eaee782670c30d08d5479d
Carsten Rosenberg <c.rosenberg@heinlein-support.de>
2021-01-07 19:15:27 +0100

    [Minor] force_actions - maybe disable current action bypass

--- src/plugins/lua/force_actions.lua
+++ src/plugins/lua/force_actions.lua
@@ -91,7 +91,9 @@ local function gen_cb(params)
     end
 
     local cact = task:get_metric_action('default')
-
+    if not params.message and not params.subject and params.act and cact == params.act then
+      return false
+    end
     if params.honor and params.honor[cact] then
       return false
     elseif params.raction and not params.raction[cact] then
