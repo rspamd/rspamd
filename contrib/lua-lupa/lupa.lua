fe3cb3a530925750efc581483b03b48c5e565ab4
Andrew Lewis <nerf@judo.za.org>
2021-01-24 22:55:42 +0200

    [Minor] Update lua-lupa
     - Fixed bug where 'else' was not evaluated if 'elseif' is present.

--- contrib/lua-lupa/lupa.lua
+++ contrib/lua-lupa/lupa.lua
@@ -1,4 +1,4 @@
--- Copyright 2015-2019 Mitchell mitchell.att.foicica.com. See LICENSE.
+-- Copyright 2015-2020 Mitchell. See LICENSE.
 -- Sponsored by the Library of the University of Antwerp.
 -- Contributions from Ana Balan.
 -- Lupa templating engine.
@@ -457,15 +457,18 @@ local function evaluate(ast, env)
       if eval(block.expression, env) then
         chunks[#chunks + 1] = evaluate(block, env)
       else
+        local evaluate_else = true
         local elseifs = block['elseif']
         if elseifs then
           for j = 1, #elseifs do
             if eval(elseifs[j].expression, env) then
               chunks[#chunks + 1] = evaluate(elseifs[j], env)
+              evaluate_else = false
               break
             end
           end
-        elseif block['else'] then
+        end
+        if evaluate_else and block['else'] then
           chunks[#chunks + 1] = evaluate(block['else'], env)
         end
       end
