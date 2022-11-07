78936996d6669561a9ac8c750ec78486b6b8f91b
Petr Vaněk <arkamar@atlas.cz>
2022-11-07 09:25:40 +0100

    [Test] Do not use hardcoded path for sqlite3 tests

--- test/lua/unit/sqlite3.lua
+++ test/lua/unit/sqlite3.lua
@@ -1,18 +1,19 @@
 context("Sqlite3 API", function()
   local sqlite3 = require "rspamd_sqlite3"
+  local tmpdir = os.getenv("TMPDIR") or "/tmp"
   
   test("Sqlite3 open", function()
-    os.remove('/tmp/rspamd_unit_test_sqlite3.sqlite')
-    local db = sqlite3.open('/tmp/rspamd_unit_test_sqlite3.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3.sqlite')
+    local db = sqlite3.open(tmpdir .. '/rspamd_unit_test_sqlite3.sqlite')
     assert_not_nil(db, "should be able to create sqlite3 db")
     db = sqlite3.open('/non/existent/path/rspamd_unit_test_sqlite3.sqlite')
     assert_nil(db, "should not be able to create sqlite3 db")
-    os.remove('/tmp/rspamd_unit_test_sqlite3.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3.sqlite')
   end)
 
   test("Sqlite3 query", function()
-    os.remove('/tmp/rspamd_unit_test_sqlite3-1.sqlite')
-    local db = sqlite3.open('/tmp/rspamd_unit_test_sqlite3-1.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3-1.sqlite')
+    local db = sqlite3.open(tmpdir .. '/rspamd_unit_test_sqlite3-1.sqlite')
     assert_not_nil(db, "should be able to create sqlite3 db")
     
     local ret = db:sql([[
@@ -23,12 +24,12 @@ context("Sqlite3 API", function()
       INSERT INTO x VALUES (?1, ?2);
     ]], 1, 'test')
     assert_true(ret, "should be able to insert row")
-    os.remove('/tmp/rspamd_unit_test_sqlite3-1.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3-1.sqlite')
   end)
 
   test("Sqlite3 rows", function()
-    os.remove('/tmp/rspamd_unit_test_sqlite3-2.sqlite')
-    local db = sqlite3.open('/tmp/rspamd_unit_test_sqlite3-2.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3-2.sqlite')
+    local db = sqlite3.open(tmpdir .. '/rspamd_unit_test_sqlite3-2.sqlite')
     assert_not_nil(db, "should be able to create sqlite3 db")
     
     local ret = db:sql([[
@@ -44,6 +45,6 @@ context("Sqlite3 API", function()
       assert_equal(row.id, '1')
       assert_equal(row.value, 'test')
     end
-    os.remove('/tmp/rspamd_unit_test_sqlite3-2.sqlite')
+    os.remove(tmpdir .. '/rspamd_unit_test_sqlite3-2.sqlite')
   end)
 end)
