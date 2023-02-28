8e7926d43c11725c93084b3c3ebd22ef1ed3d689
moisseev <moiseev@mezonplus.ru>
2023-02-28 10:25:29 +0300

    [WebUI] Update JS libraries

--- interface/js/main.js
+++ interface/js/main.js
@@ -4,7 +4,7 @@ requirejs.config({
     baseUrl: "js/lib",
     paths: {
         app: "../app",
-        jquery: "jquery-3.6.1.min",
+        jquery: "jquery-3.6.3.min",
         visibility: "visibility.min",
         bootstrap: "bootstrap.bundle.min",
         codejar: "codejar.min",
