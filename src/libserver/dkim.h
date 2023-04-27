529a1f8b4767ee8a84daaea6f6d825bb4793e1f3
Vsevolod Stakhov <vsevolod@rspamd.com>
2023-04-27 21:40:59 +0100

    [Minor] Remove unused macro

--- src/libserver/dkim.h
+++ src/libserver/dkim.h
@@ -34,51 +34,24 @@
 /* Errors (from OpenDKIM) */
 
 #define DKIM_SIGERROR_UNKNOWN       (-1)    /* unknown error */
-#define DKIM_SIGERROR_OK        0   /* no error */
 #define DKIM_SIGERROR_VERSION       1   /* unsupported version */
-#define DKIM_SIGERROR_DOMAIN        2   /* invalid domain (d=/i=) */
 #define DKIM_SIGERROR_EXPIRED       3   /* signature expired */
 #define DKIM_SIGERROR_FUTURE        4   /* signature in the future */
-#define DKIM_SIGERROR_TIMESTAMPS    5   /* x= < t= */
 #define DKIM_SIGERROR_NOREC         6   /* No record */
 #define DKIM_SIGERROR_INVALID_HC    7   /* c= invalid (header) */
 #define DKIM_SIGERROR_INVALID_BC    8   /* c= invalid (body) */
-#define DKIM_SIGERROR_MISSING_A     9   /* a= missing */
 #define DKIM_SIGERROR_INVALID_A     10  /* a= invalid */
-#define DKIM_SIGERROR_MISSING_H     11  /* h= missing */
 #define DKIM_SIGERROR_INVALID_L     12  /* l= invalid */
-#define DKIM_SIGERROR_INVALID_Q     13  /* q= invalid */
-#define DKIM_SIGERROR_INVALID_QO    14  /* q= option invalid */
-#define DKIM_SIGERROR_MISSING_D     15  /* d= missing */
 #define DKIM_SIGERROR_EMPTY_D       16  /* d= empty */
-#define DKIM_SIGERROR_MISSING_S     17  /* s= missing */
 #define DKIM_SIGERROR_EMPTY_S       18  /* s= empty */
-#define DKIM_SIGERROR_MISSING_B     19  /* b= missing */
 #define DKIM_SIGERROR_EMPTY_B       20  /* b= empty */
-#define DKIM_SIGERROR_CORRUPT_B     21  /* b= corrupt */
 #define DKIM_SIGERROR_NOKEY     22  /* no key found in DNS */
-#define DKIM_SIGERROR_DNSSYNTAX     23  /* DNS reply corrupt */
 #define DKIM_SIGERROR_KEYFAIL       24  /* DNS query failed */
-#define DKIM_SIGERROR_MISSING_BH    25  /* bh= missing */
 #define DKIM_SIGERROR_EMPTY_BH      26  /* bh= empty */
-#define DKIM_SIGERROR_CORRUPT_BH    27  /* bh= corrupt */
 #define DKIM_SIGERROR_BADSIG        28  /* signature mismatch */
-#define DKIM_SIGERROR_SUBDOMAIN     29  /* unauthorized subdomain */
-#define DKIM_SIGERROR_MULTIREPLY    30  /* multiple records returned */
 #define DKIM_SIGERROR_EMPTY_H       31  /* h= empty */
 #define DKIM_SIGERROR_INVALID_H     32  /* h= missing req'd entries */
-#define DKIM_SIGERROR_TOOLARGE_L    33  /* l= value exceeds body size */
-#define DKIM_SIGERROR_MBSFAILED     34  /* "must be signed" failure */
-#define DKIM_SIGERROR_KEYVERSION    35  /* unknown key version */
-#define DKIM_SIGERROR_KEYUNKNOWNHASH    36  /* unknown key hash */
 #define DKIM_SIGERROR_KEYHASHMISMATCH   37  /* sig-key hash mismatch */
-#define DKIM_SIGERROR_NOTEMAILKEY   38  /* not an e-mail key */
-#define DKIM_SIGERROR_UNUSED2       39  /* OBSOLETE */
-#define DKIM_SIGERROR_KEYTYPEMISSING    40  /* key type missing */
-#define DKIM_SIGERROR_KEYTYPEUNKNOWN    41  /* key type unknown */
-#define DKIM_SIGERROR_KEYREVOKED    42  /* key revoked */
-#define DKIM_SIGERROR_KEYDECODE     43  /* key couldn't be decoded */
-#define DKIM_SIGERROR_MISSING_V     44  /* v= tag missing */
 #define DKIM_SIGERROR_EMPTY_V       45  /* v= tag empty */
 
 #ifdef  __cplusplus
