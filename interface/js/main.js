0259a4f5cd93cf9679f82f479ec007eb73e83dc1
moisseev <moiseev@mezonplus.ru>
2023-08-08 10:41:18 +0300

    [Minor] Update JS libraries

--- interface/js/main.js
+++ interface/js/main.js
@@ -4,7 +4,7 @@ requirejs.config({
     baseUrl: "js/lib",
     paths: {
         app: "../app",
-        jquery: "jquery-3.6.3.min",
+        jquery: "jquery-3.7.0.min",
         visibility: "visibility.min",
         bootstrap: "bootstrap.bundle.min",
         codejar: "codejar.min",
