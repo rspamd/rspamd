1fb6291a1e7fd47930958349e5ff1a506f5074e1
Lyuben Bahtarliev <>
2023-09-11 06:28:47 +0300

    [Feature] Add ICAP Filename and Content-Type from TODO List

--- lualib/lua_scanners/icap.lua
+++ lualib/lua_scanners/icap.lua
@@ -240,10 +240,13 @@ local function icap_check(task, content, digest, rule, maybe_part)
 
       local function get_req_headers()
 
+        local in_client_ip = task:get_from_ip()
+        lua_util.debugm(rule.name, task, 'URL: http://%s/%s | Content-Type: %s/%s', in_client_ip, maybe_part[1], maybe_part[2], maybe_part[3])
+
         local req_hlen = 2
-        table.insert(req_headers, string.format('GET %s HTTP/1.0\r\n', rule.req_fake_url))
+        table.insert(req_headers, string.format('GET http://%s/%s HTTP/1.0\r\n', in_client_ip, maybe_part[1]))
         table.insert(req_headers, string.format('Date: %s\r\n', rspamd_util.time_to_string(rspamd_util.get_time())))
-        --table.insert(http_headers, string.format('Content-Type: %s\r\n', 'text/html'))
+        table.insert(http_headers, string.format('Content-Type: %s/%s\r\n', maybe_part[2], maybe_part[3]))
         if rule.user_agent ~= "none" then
           table.insert(req_headers, string.format("User-Agent: %s\r\n", rule.user_agent))
         end
