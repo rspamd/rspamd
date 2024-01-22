rspamd/rspamd

commit b189c9fea6334cc9211b73d76acd05e7f1f71913

[Minor] lua_maps docs: apply formatting

lualib/lua_maps.lua
@@ -553,17 +553,17 @@ exports.rspamd_maybe_check_map = rspamd_maybe_check_map
 -- @function lua_maps.fill_config_maps(mname, options, defs)
 -- Fill maps that could be defined in defs, from the config in the options
 -- Defs is a table indexed by a map's parameter name and defining it's config,
--- for example:
-defs = {
-  my_map = {
-    type = 'map',
-    description = 'my cool map',
-    optional = true,
-  }
-}
--- Then this function will look for opts.my_map parameter and try to replace it's with
+-- @example
+-- defs = {
+--   my_map = {
+--     type = 'map',
+--     description = 'my cool map',
+--     optional = true,
+--   }
+-- }
+-- --[[ Then this function will look for opts.my_map parameter and try to replace it's with
 -- a map with the specific type, description but not failing if it was empty.
--- It will also set options.my_map_orig to the original value defined in the map
+-- It will also set options.my_map_orig to the original value defined in the map --]]
 --]]
 exports.fill_config_maps = function(mname, opts, map_defs)
   assert(type(opts) == 'table')

commit aaf4373418969e0011762c3e01697116f483ecc2

[Fix] Use short comments rather than long one
Rspamd fails to start with Lua-5.1 because long comment does not allow
nesting, see [1]. This change replaces it to short comments in order to
fix the issue but preserves this part as comment in docs.

[1] https://www.lua.org/manual/5.1/manual.html#7.1

Bug: https://bugs.gentoo.org/922522
Fixes: b189c9fea633 ("[Minor] lua_maps docs: apply formatting")
Fixes: https://github.com/rspamd/rspamd/issues/4784

lualib/lua_maps.lua
@@ -561,9 +561,9 @@ exports.rspamd_maybe_check_map = rspamd_maybe_check_map
 --     optional = true,
 --   }
 -- }
--- --[[ Then this function will look for opts.my_map parameter and try to replace it's with
--- a map with the specific type, description but not failing if it was empty.
--- It will also set options.my_map_orig to the original value defined in the map --]]
+-- -- Then this function will look for opts.my_map parameter and try to replace it's with
+-- -- a map with the specific type, description but not failing if it was empty.
+-- -- It will also set options.my_map_orig to the original value defined in the map.
 --]]
 exports.fill_config_maps = function(mname, opts, map_defs)
   assert(type(opts) == 'table')