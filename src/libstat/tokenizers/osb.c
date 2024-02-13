dca0a49e6d35f157e7ffdf8b0f3e61f707013e09
Vsevolod Stakhov <vsevolod@rspamd.com>
2024-02-13 15:04:24 +0000

    [Fix] Make stat tokens allocation consistent

--- src/libstat/tokenizers/osb.c
+++ src/libstat/tokenizers/osb.c
@@ -1,11 +1,11 @@
-/*-
- * Copyright 2016 Vsevolod Stakhov
+/*
+ * Copyright 2024 Vsevolod Stakhov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -302,9 +302,8 @@ gint rspamd_tokenizer_osb(struct rspamd_stat_ctx *ctx,
 		hashpipe[i].h = 0xfe;
 		hashpipe[i].t = NULL;
 	}
-
 	token_size = sizeof(rspamd_token_t) +
-				 sizeof(gdouble) * ctx->statfiles->len;
+				 sizeof(RSPAMD_TOKEN_VALUE_TYPE) * ctx->statfiles->len;
 	g_assert(token_size > 0);
 
 	for (w = 0; w < words->len; w++) {
