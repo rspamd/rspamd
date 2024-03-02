29bdc2c18201b202609d7b355814d948da791e71
Ivan Stakhov <50211739+LeftTry@users.noreply.github.com>
2024-03-02 18:29:17 +0600

    Clean up

--- cmake-build-debug/utils/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fum/CLionProjects/rspamd/utils
-# Build directory: /home/fum/CLionProjects/rspamd/cmake-build-debug/utils
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
