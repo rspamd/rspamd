0b97e9487ed53ed8bed85e9a3904d3800f7fc85b
Vsevolod Stakhov <vsevolod@rspamd.com>
2024-03-14 15:20:29 +0000

    [Test] Another try to fight with Python
    It is probably easier to learn Chinese than to understand this language

--- test/functional/lib/rspamd.py
+++ test/functional/lib/rspamd.py
@@ -12,6 +12,17 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 from urllib.request import urlopen
 import glob
@@ -34,7 +45,7 @@ import json
 
 
 def Check_JSON(j):
-    d = json.JSONDecoder(strict=True).decode(j)
+    d = json.JSONDecoder(strict=True).decode(j.decode('utf-8'))
     logger.debug('got json %s' % d)
     assert len(d) > 0
     assert 'error' not in d
@@ -43,7 +54,7 @@ def Check_JSON(j):
 
 def check_json_log(fn):
     line_count = 0
-    f = open(fn, 'r')
+    f = open(fn, 'r', encoding="utf-8")
     for l in f.readlines():
         d = json.JSONDecoder(strict=True).decode(l)
         assert len(d) > 0
