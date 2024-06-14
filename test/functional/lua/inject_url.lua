43f18ea63887e7e8e959778bb0550230f7ca048e
LeftTry <lerest.go@gmail.com>
2024-06-14 17:25:28 +0500

    [Minor] Clean up

--- test/functional/lua/inject_url.lua
+++ test/functional/lua/inject_url.lua
@@ -1,7 +1,6 @@
 local rspamd_url = require('rspamd_url')
 
 local function task_inject_cb (task)
-    local l = {}
     local url = rspamd_url:create(task:get_mempool(), 'http://example.com?redir=http://another.com')
     task:inject_url(url)
     return true
