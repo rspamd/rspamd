c767326facfeefc388f758b67a95a23040ee20c8
LeftTry <lerest.go@gmail.com>
2024-06-15 16:42:06 +0500

    [Test] Test base task:inject

--- test/functional/lua/inject_url.lua
+++ test/functional/lua/inject_url.lua
@@ -3,10 +3,10 @@ local lua_util = require('lua_util')
 
 local function task_inject_cb (task)
     local url_text = 'http://example.com?redir=http://another.com'
-    lua_util.debugm('inject', task, 'URL')
     local url_to_inject = url.create(task:get_mempool(), url_text)
     lua_util.debugm('inject', task, 'LRU')
     task:inject_url(url_to_inject)
+    lua_util.debugm('inject', task, 'URL')
     return true
 end
 
