33d197ba572c0d60fe19ce43f51858562e5b9834
LeftTry <lerest.go@gmail.com>
2024-06-15 16:32:42 +0500

    [Minor] Debug task:inject_url test

--- test/functional/lua/inject_url.lua
+++ test/functional/lua/inject_url.lua
@@ -1,8 +1,11 @@
 local url = require('rspamd_url')
+local lua_util = require('lua_util')
 
 local function task_inject_cb (task)
     local url_text = 'http://example.com?redir=http://another.com'
+    lua_util.debugm('inject', task, 'URL')
     local url_to_inject = url.create(task:get_mempool(), url_text)
+    lua_util.debugm('inject', task, 'LRU')
     task:inject_url(url_to_inject)
     return true
 end
