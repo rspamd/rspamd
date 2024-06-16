ef2d5dc9edfe741997f62866ad0e8c151d80f1b4
LeftTry <lerest.go@gmail.com>
2024-06-16 15:05:39 +0500

    [Test] Fix task:inject

--- src/lua/lua_task.c
+++ src/lua/lua_task.c
@@ -2655,7 +2655,13 @@ lua_task_inject_url(lua_State *L)
 		if (rspamd_url_set_add_or_increase(MESSAGE_FIELD(task, urls), url->url, false)) {
 			if (mpart && mpart->urls) {
 				/* Also add url to the mime part */
-				g_ptr_array_add(mpart->urls, url->url);
+				//g_ptr_array_add(mpart->urls, url->url);
+				mime_text_part->mime_part = mpart;
+				g_byte_array_append(mime_text_part->utf_stripped_content, url->url->string, url->url->urllen);
+				rspamd_url_text_extract(task->task_pool, task,
+										mime_text_part,
+										&(url->url->order),
+										RSPAMD_URL_FIND_ALL);
 			}
 		}
 	}
@@ -2668,16 +2674,10 @@ lua_task_inject_url(lua_State *L)
 	//mime_text_part->exceptions = g_list_alloc();
 	//mime_text_part->utf_stripped_content->len = url->url->urllen;
 	//mime_text_part->newlines = 0;
-	mime_text_part->mime_part = mpart;
 	if(url->url->querylen > 0) {
 		if (task && task->message && url && url->url) {
 			if (rspamd_url_set_add_or_increase(MESSAGE_FIELD(task, urls), url->url, false)) {
 				if (mime_text_part->mime_part && mime_text_part->mime_part->urls) {
-					g_byte_array_append(mime_text_part->utf_stripped_content, url->url->string, url->url->urllen);
-					rspamd_url_text_extract(task->task_pool, task,
-											mime_text_part,
-											&(url->url->order),
-											RSPAMD_URL_FIND_ALL);
 				}
 			}
 		}
