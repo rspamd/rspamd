d32f3e3b7c1e58a612e0f3b8d3e36130bb5b67c1
LeftTry <lerest.go@gmail.com>
2024-06-18 11:53:18 +0500

    [Test] Fix task:inject_url

--- src/lua/lua_task.c
+++ src/lua/lua_task.c
@@ -2674,7 +2674,7 @@ void find_urls(struct rspamd_lua_url* url, struct rspamd_mime_text_part *mpart, 
 																		 sizeof(struct rspamd_url));
 					url_parsed->raw = rspamd_mempool_alloc(task->task_pool, i - start + 1);
 					for(int j = start;j < i;j++) {
-						strcat(url_parsed->raw, &url->url->raw[j]);
+						url_parsed->raw[j - start] = url->url->raw[j];
 					}
 					g_ptr_array_add(mpart->mime_part->urls, url_parsed);
 					start = i;
@@ -2686,7 +2686,7 @@ void find_urls(struct rspamd_lua_url* url, struct rspamd_mime_text_part *mpart, 
 																 sizeof(struct rspamd_url));
 			url_parsed->raw = rspamd_mempool_alloc(task->task_pool, url_len - start + 1);
 			for (int j = start; j < url_len; j++) {
-				strcat(url_parsed->raw, &url->url->raw[j]);
+				url_parsed->raw[j - start] = url->url->raw[j];
 			}
 			g_ptr_array_add(mpart->mime_part->urls, url_parsed);
 		}
