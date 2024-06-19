3d2dc4a58703207e35916d3d79b32424401947a7
LeftTry <lerest.go@gmail.com>
2024-06-19 19:24:23 +0500

    [Test] Fix inject url

--- src/lua/lua_task.c
+++ src/lua/lua_task.c
@@ -2735,9 +2735,8 @@ lua_task_inject_url(lua_State *L)
 									RSPAMD_URL_FIND_ALL);
 			*/
 			if(mpart == NULL) {
-				struct rspamd_mime_part *mime_part = rspamd_mempool_alloc0(task->task_pool,
-																		   sizeof(struct rspamd_mime_part));
-				mime_part->urls = g_ptr_array_new();
+				mpart = rspamd_mempool_alloc0(task->task_pool, sizeof(struct rspamd_mime_part));
+				mpart->urls = g_ptr_array_new();
 			}
 			inject_url_query(task, url->url, mpart->urls);
 		}
