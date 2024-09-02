becd7c3afeb0abc2f02d5c0ab27a1f1624f53e98
LeftTry <lerest.go@gmail.com>
2024-09-02 15:03:15 +0600

    [Test] Add tests for maybe encode/decode header

---
+++ test/lua/unit/lua_util.maybe_encode_decode_header.lua
@@ -0,0 +1,23 @@
+local util = require 'lua_util'
+
+local settings = {
+    prefix = 'prefix',
+    encrypt = true,
+    publickey = 'public_key'
+}
+
+context("Lua util - maybe encode/decode header", function()
+    local header = 'X-Spamd-Result'
+    local encoded_header = ''
+    local decoded_header = ''
+
+    test("Encode/Decode header", function()
+        encoded_header = util.maybe_encode_header(header, settings, settings.prefix)
+        decoded_header = util.maybe_decode_header(encoded_header. settings. settings.prefix)
+        if header ~= decoded_header then
+            assert_true(false, 'Failed to confirm equality of original header and decoded one')
+        else
+            assert_true(true, 'Succeed to confirm equality of original header and decoded header')
+        end
+    end)
+end)
