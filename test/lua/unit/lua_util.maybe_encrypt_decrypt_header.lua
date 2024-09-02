cc16710998939b3d88a0eeef93634fee0044f0af
LeftTry <lerest.go@gmail.com>
2024-09-03 00:56:45 +0600

    [Minor] Little fix

--- test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
+++ test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
@@ -2,24 +2,28 @@ local util = require 'lua_util'
 
 local settings = {
     prefix = 'prefix',
-    encrypt = true,
-    key = 'key'
+    prefix_encrypt = true,
+    prefix_key = 'key'
 }
 
 context("Lua util - maybe encrypt/decrypt header", function()
     test("Encrypt/Decrypt header", function()
         local header = 'X-Spamd-Result'
 
-        local encoded_header = util.maybe_encrypt_header(header, settings, settings.prefix)
-        if encoded_header == header then
-            assert_true(false, 'Failed to encode header')
+        local encrypted_header = util.maybe_encrypt_header(header, settings, settings.prefix)
+        if encrypted_header == header or encrypted_header == nil then
+            assert_true(false, 'Failed to encrypt header')
         end
-        local decoded_header = util.maybe_decrypt_header(encoded_header, settings, settings.prefix)
 
-        if header ~= decoded_header then
-            assert_true(false, 'Failed to confirm equality of original header and decoded one')
+        local decrypted_header = util.maybe_decrypt_header(encrypted_header, settings, settings.prefix)
+        if decrypted_header == encrypted_header or decrypted_header == nil then
+            assert_true(false, 'Failed to decrypt header')
+        end
+
+        if header ~= decrypted_header then
+            assert_true(false, 'Failed to confirm equality of original header and decrypted one')
         else
-            assert_true(true, 'Succeed to confirm equality of original header and decoded header')
+            assert_true(true, 'Succeed to confirm equality of original header and decrypted header')
         end
     end)
 end)
