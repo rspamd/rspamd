84d7297b2956bf6c9eee5c039026f771c48d1ebf
LeftTry <lerest.go@gmail.com>
2024-09-03 01:42:49 +0600

    [Test] Add test for nonce

--- test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
+++ test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
@@ -1,22 +1,48 @@
 local util = require 'lua_util'
 
-local settings = {
-    prefix = 'prefix',
-    prefix_encrypt = true,
-    prefix_key = 'key'
-}
-
 context("Lua util - maybe encrypt/decrypt header", function()
-    test("Encrypt/Decrypt header", function()
+    test("Encrypt/Decrypt header with nonce", function()
         local header = 'X-Spamd-Result'
+        local settings = {
+            prefix = 'prefix',
+            prefix_encrypt = true,
+            prefix_key = 'key',
+            prefix_nonce = 'nonce'
+        }
 
         local encrypted_header = util.maybe_encrypt_header(header, settings, settings.prefix)
         if encrypted_header == header or encrypted_header == nil then
             assert_true(false, 'Failed to encrypt header')
         end
 
-        local decrypted_header = util.maybe_decrypt_header(encrypted_header, settings, settings.prefix)
-        if decrypted_header == encrypted_header or decrypted_header == nil then
+        local result, decrypted_header = util.maybe_decrypt_header(encrypted_header, settings, settings.prefix)
+        if result or decrypted_header == encrypted_header or decrypted_header == nil then
+            assert_true(false, 'Failed to decrypt header')
+        end
+
+        if header ~= decrypted_header then
+            assert_true(false, 'Failed to confirm equality of original header and decrypted one')
+        else
+            assert_true(true, 'Succeed to confirm equality of original header and decrypted header')
+        end
+    end)
+
+    test("Encrypt/Decrypt header without nonce", function()
+        local header = 'X-Spamd-Result'
+        local settings = {
+            prefix = 'prefix',
+            prefix_encrypt = true,
+            prefix_key = 'key'
+        }
+
+        local encrypted_header, nonce = util.maybe_encrypt_header(header, settings, settings.prefix)
+        if encrypted_header == header or encrypted_header == nil then
+            assert_true(false, 'Failed to encrypt header')
+        end
+
+        local result, decrypted_header = util.maybe_decrypt_header(encrypted_header, settings,
+                settings.prefix, nonce)
+        if result or decrypted_header == encrypted_header or decrypted_header == nil then
             assert_true(false, 'Failed to decrypt header')
         end
 
