c803725508ca5f819edbb7e2dbdd055afe5a3294
LeftTry <lerest.go@gmail.com>
2024-09-03 00:46:44 +0600

    [Minor] Small fix

--- test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
+++ test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
@@ -3,18 +3,18 @@ local util = require 'lua_util'
 local settings = {
     prefix = 'prefix',
     encrypt = true,
-    publickey = 'public_key'
+    key = 'key'
 }
 
-context("Lua util - maybe encode/decode header", function()
-    test("Encode/Decode header", function()
+context("Lua util - maybe encrypt/decrypt header", function()
+    test("Encrypt/Decrypt header", function()
         local header = 'X-Spamd-Result'
 
-        local encoded_header = util.maybe_encode_header(header, settings, settings.prefix)
+        local encoded_header = util.maybe_encrypt_header(header, settings, settings.prefix)
         if encoded_header == header then
             assert_true(false, 'Failed to encode header')
         end
-        local decoded_header = util.maybe_decode_header(encoded_header. settings. settings.prefix)
+        local decoded_header = util.maybe_decrypt_header(encoded_header. settings. settings.prefix)
 
         if header ~= decoded_header then
             assert_true(false, 'Failed to confirm equality of original header and decoded one')
