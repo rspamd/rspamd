7245aed81ad48ec129d6415025a09a1b2935f38b
LeftTry <lerest.go@gmail.com>
2024-09-15 20:06:08 +0600

    [Minor] Small upgrade to debug

--- utils/encrypt_decrypt_header.py
+++ utils/encrypt_decrypt_header.py
@@ -10,49 +10,45 @@ def create_secret_box(key):
     box = SecretBox(key)
     return box
 
-def encrypt_header(header, key, nonce):
+def encrypt_text(header, key, nonce):
     box = create_secret_box(key)
     encrypted_header = box.encrypt(header, nonce=nonce)
     return encrypted_header
 
-def decrypt_header(encrypted_header, key):
+def decrypt_text(encrypted_header, key):
     box = create_secret_box(key)
     decrypted_header = box.decrypt(encrypted_header)
     return decrypted_header
 
 def main():
-    parser = argparse.ArgumentParser(description="Encrypt or Decrypt a header.")
+    parser = argparse.ArgumentParser(description="Encrypt or Decrypt a text.")
     subparsers = parser.add_subparsers(dest="command", help="encrypt or decrypt")
 
-    encrypt_parser = subparsers.add_parser("encrypt", help="Encrypt a message")
-    encrypt_parser.add_argument("--header", type=str, required=True, help="Header to encrypt")
+    encrypt_parser = subparsers.add_parser("encrypt", help="Encrypt a text")
+    encrypt_parser.add_argument("--text", type=str, required=True, help="Text to encrypt")
     encrypt_parser.add_argument("--key", type=str, required=True, help="Encryption key")
     encrypt_parser.add_argument("--nonce", type=str, required=True, help="Encryption nonce")
 
-    decrypt_parser = subparsers.add_parser("decrypt", help="Decrypt a message")
-    decrypt_parser.add_argument("--encrypted_header", type=str, required=True, help="Encrypted header")
+    decrypt_parser = subparsers.add_parser("decrypt", help="Decrypt a text")
+    decrypt_parser.add_argument("--encrypted_text", type=str, required=True, help="Encrypted text")
     decrypt_parser.add_argument("--key", type=str, required=True, help="Decryption key")
 
     args = parser.parse_args()
 
     if args.command == "encrypt":
-        header = args.header.encode()
+        text = args.text.encode()
         key = args.key.encode()
         nonce = base64.b64decode(args.nonce)
 
-        encrypted_header = encrypt_header(header, key, nonce)
-        print(encrypted_header)
-        print(len(encrypted_header))
-        print("Encrypted header (base 64):", base64.b64encode(encrypted_header))
-        print("Ciphertext (base 64):", base64.b64encode(encrypted_header.ciphertext))
-        print("Nonce (base 64):", base64.b64encode(encrypted_header.nonce))
+        encrypted_text = encrypt_text(text, key, nonce)
+        print("Encrypted text (base 64):", base64.b64encode(encrypted_text))
 
     elif args.command == "decrypt":
-        encrypted_header = base64.b64decode(args.encrypted_header)
+        encrypted_text = base64.b64decode(args.encrypted_text)
         key = args.key.encode()
 
-        decrypted_header = decrypt_header(encrypted_header, key)
-        print("Decrypted message:", decrypted_header.decode())
+        decrypted_text = decrypt_text(encrypted_text, key)
+        print("Decrypted text:", decrypted_text.decode())
 
 if __name__ == "__main__":
     main()
