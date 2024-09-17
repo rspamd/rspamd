d8b38cb8a8ceab9533857239a846d971a92f3df0
LeftTry <lerest.go@gmail.com>
2024-09-17 15:51:07 +0600

    [Test] Small fix

--- test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
+++ test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
@@ -15,8 +15,8 @@ context("Lua util - maybe encrypt/decrypt header", function()
             assert_true(false, 'Failed to encrypt header')
         end
 
-        settings.prefix_nonce = string.sub(tostring(encrypted_header), 1, 24)
-        local text = string.sub(tostring(encrypted_header), 25)
+        --settings.prefix_nonce = string.sub(tostring(encrypted_header), 1, 24)
+        local text = string.sub(tostring(encrypted_header), 6)
         local decrypted_header = util.maybe_decrypt_header(text, settings, settings.prefix)
         if decrypted_header == encrypted_header or decrypted_header == nil then
             assert_true(false, 'Failed to decrypt header')
