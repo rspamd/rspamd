a36a474f217d8f593e21eda3ac99fe1fd87d5f4a
LeftTry <lerest.go@gmail.com>
2024-09-17 13:59:12 +0600

    [Minor] Provide full support for new return format of maybe_encrypt_header

--- test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
+++ test/lua/unit/lua_util.maybe_encrypt_decrypt_header.lua
@@ -15,7 +15,8 @@ context("Lua util - maybe encrypt/decrypt header", function()
             assert_true(false, 'Failed to encrypt header')
         end
 
-        local decrypted_header = util.maybe_decrypt_header(encrypted_header, settings, settings.prefix)
+        local text = string.sub(tostring(encrypted_header), 25)
+        local decrypted_header = util.maybe_decrypt_header(text, settings, settings.prefix)
         if decrypted_header == encrypted_header or decrypted_header == nil then
             assert_true(false, 'Failed to decrypt header')
         end
