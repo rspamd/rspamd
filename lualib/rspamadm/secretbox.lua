372770d16bb3366f2fabac77c998646563393c62
LeftTry <lerest.go@gmail.com>
2024-09-17 13:28:13 +0600

    [Minor] Add flag to manage encodings

--- lualib/rspamadm/secretbox.lua
+++ lualib/rspamadm/secretbox.lua
@@ -3,38 +3,85 @@ local rspamd_util = require 'rspamd_util'
 local argparse = require 'argparse'
 
 local parser = argparse()
-    :name "rspamadm encrypt/decrypt text"
+    :name "rspamadm secretbox"
     :description "Encrypt/Decrypt given text with given key and nonce"
     :help_description_margin(32)
     :command_target('command')
     :require_command(true)
 
+parser:mutex(
+    parser:flag '-R --raw'
+          :description('Encrypted text(and nonce if it is there) will be given in raw'),
+    parser:flag '-H --hex'
+          :description('Encrypted text(and nonce if it is there) will be given in hex'),
+    parser:flag '-b --base32'
+          :description('Encrypted text(and nonce if it is there) will be given in base32'),
+    parser:flag '-B --base64'
+          :description('Encrypted text(and nonce if it is there) will be given in base64')
+)
+
 local decrypt = parser:command 'decrypt'
                       :description 'Decrypt text with given key and nonce'
 
 decrypt:option "-t --text"
-      :description("Encrypted text(Base 64)")
-      :argname("<text>")
+       :description("Encrypted text(Base 64)")
+       :argname("<text>")
 decrypt:option "-k --key"
-      :description("Key used to encrypt text")
-      :argname("<key>")
+       :description("Key used to encrypt text")
+       :argname("<key>")
 decrypt:option "-n --nonce"
-      :description("Nonce used to encrypt text(Base 64)")
-      :argname("<nonce>")
-      :default('nil')
+       :description("Nonce used to encrypt text(Base 64)")
+       :argname("<nonce>")
+       :default(nil)
 
 local encrypt = parser:command 'encrypt'
                       :description 'Encrypt text with given key'
 
 encrypt:option "-t --text"
-      :description("Text to encrypt")
-      :argname("<text>")
+       :description("Text to encrypt")
+       :argname("<text>")
 encrypt:option "-k --key"
-      :description("Key to encrypt text")
-      :argname("<key>")
+       :description("Key to encrypt text")
+       :argname("<key>")
 encrypt:option "-n --nonce"
        :description("Nonce to encrypt text(Base 64)")
        :argname("<nonce>")
+       :default(nil)
+
+function string.fromhex(str)
+    return (str:gsub('..', function (cc)
+        return string.char(tonumber(cc, 16))
+    end))
+end
+
+function string.tohex(str)
+    return (str:gsub('.', function (c)
+        return string.format('%02X', string.byte(c))
+    end))
+end
+
+local function set_up_encoding(args, type, text)
+    local text_ = text
+
+    if type == 'encode' then
+        if args.hex then
+            text_ = text:tohex()
+        elseif args.base32 then
+            text_ = rspamd_util.encode_base32(text)
+        elseif args.base64 then
+            text_ = rspamd_util.encode_base64(text)
+        end
+    elseif type == 'decode' then
+        if args.hex then
+            text_ = text:fromhex()
+        elseif args.base32 then
+            text_ = rspamd_util.decode_base32(text)
+        elseif args.base64 then
+            text_ = rspamd_util.decode_base64(text)
+        end
+    end
+    return text_
+end
 
 local function decryption_handler(args)
     local settings = {
@@ -42,16 +89,21 @@ local function decryption_handler(args)
         dec_encrypt = true,
         dec_key = args.key
     }
+
     local decrypted_header = ''
-    if(args.nonce ~= 'nil') then
-         decrypted_header = util.maybe_decrypt_header(rspamd_util.decode_base64(args.text), settings, settings.prefix,
-                rspamd_util.decode_base64(args.nonce))
+    if(args.nonce ~= nil) then
+        local decoded_text = set_up_encoding(args, 'decode', tostring(args.text))
+        local decoded_nonce = set_up_encoding(args, 'decode', tostring(args.nonce))
+
+        decrypted_header = util.maybe_decrypt_header(decoded_text, settings, settings.prefix, decoded_nonce)
     else
-        local nonce = string.sub(args.text, 1, 32)
-        local text = string.sub(args.text, 33)
-        decrypted_header = util.maybe_decrypt_header(rspamd_util.decode_base64(text), settings, settings.prefix,
-                rspamd_util.decode_base64(nonce))
+        local text_with_nonce = set_up_encoding(args, 'decode', tostring(args.text))
+        local nonce = string.sub(tostring(text_with_nonce), 1, 24)
+        local text = string.sub(tostring(text_with_nonce), 25)
+
+        decrypted_header = util.maybe_decrypt_header(text, settings, settings.prefix, nonce)
     end
+
     if decrypted_header ~= nil then
         print(decrypted_header)
     else
@@ -64,13 +116,16 @@ local function encryption_handler(args)
         prefix = 'dec',
         dec_encrypt = true,
         dec_key = args.key,
-        dec_nonce = rspamd_util.decode_base64(args.nonce)
     }
 
-    local encrypted_text, _, encrypted_text_with_nonce
-                                                    = util.maybe_encrypt_header(args.text, settings, settings.prefix)
+    if args.nonce ~= nil then
+        settings.dec_nonce = set_up_encoding(args, 'decode', tostring(args.nonce))
+    end
+
+    local encrypted_text = util.maybe_encrypt_header(args.text, settings, settings.prefix)
+
     if encrypted_text ~= nil then
-        print(rspamd_util.encode_base64(encrypted_text_with_nonce))
+        print(set_up_encoding(args, 'encode', tostring(encrypted_text)))
     else
         print('The encryption failed. Please check the correctness of the arguments given.')
     end
