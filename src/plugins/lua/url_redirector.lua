171989fcb6deb5cb964fd456e89cf04d8d7c6921
Dmitriy Alekseev <1865999+dragoangel@users.noreply.github.com>
2024-10-01 01:27:30 +0200

    Check tld in url_redirector in addition to exact host
    This align logic between `redirector` symbol in `multimap.conf` and `url_redirector.lua`

--- src/plugins/lua/url_redirector.lua
+++ src/plugins/lua/url_redirector.lua
@@ -348,6 +348,11 @@ local function url_redirector_handler(task)
     task = task,
     limit = settings.max_urls,
     filter = function(url)
+      local tld = url:get_tld()
+      if settings.redirector_hosts_map:get_key(tld) then
+        lua_util.debugm(N, task, 'check url %s', tostring(url))
+        return true
+      end
       local host = url:get_host()
       if settings.redirector_hosts_map:get_key(host) then
         lua_util.debugm(N, task, 'check url %s', tostring(url))
