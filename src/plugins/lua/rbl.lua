77d6ef226a0388de7872b1aca4230418a2597837
Mike Teplynin <mike@teplynin.com>
2024-12-13 20:20:15 +0300

    Skipping check_received function for ip address if RBL use botch checks, 'from' and 'received'

--- src/plugins/lua/rbl.lua
+++ src/plugins/lua/rbl.lua
@@ -693,12 +693,12 @@ local function gen_rbl_callback(rule)
 
     for pos, rh in ipairs(received) do
       if check_conditions(rh, pos) then
-        if rh.real_ip ~= from_ip then
+        if rh.real_ip ~= from_ip or (rh.real_ip == from_ip and not rule.from) then
           add_dns_request(task, rh.real_ip, false, true,
               requests_table, 'received',
               whitelist)
         else
-          lua_util.debugm(N, task, 'Received IP same as From IP, skipping extra check')
+          lua_util.debugm(N, task, 'rbl %s; skip check_received for %s: Received IP same as From IP and will be checked only in check_from function', rule.symbol, rh.real_ip)
         end
       end
     end
