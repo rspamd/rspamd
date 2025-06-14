4c604597f25ef4b4cff4833fde3c41b6bdc43614
kevinzb56 <143kevinshah@gmail.com>
2025-06-14 17:36:36 +0530

    updated bayes_cache_check

--- lualib/redis_scripts/bayes_cache_check.lua
+++ lualib/redis_scripts/bayes_cache_check.lua
@@ -2,10 +2,17 @@
 -- This script accepts the following parameters:
 -- key1 - cache id
 -- key2 - configuration table in message pack
+-- key3 - (optional) category
 
 local cache_id = KEYS[1]
 local conf = cmsgpack.unpack(KEYS[2])
+local category = KEYS[3]
 cache_id = string.sub(cache_id, 1, conf.cache_elt_len)
+
+local prefix_base = conf.cache_prefix
+if category then
+  prefix_base = prefix_base .. "_" .. category
+end
 
 -- Try each prefix that is in Redis
 for i = 0, conf.cache_max_keys do
