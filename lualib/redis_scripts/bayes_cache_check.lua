4734b8b9ee6d0cd27bff01b390bbfd8c59a83b57
kevinzb56 <143kevinshah@gmail.com>
2025-06-23 17:18:50 +0530

    added category as a lua table

--- lualib/redis_scripts/bayes_cache_check.lua
+++ lualib/redis_scripts/bayes_cache_check.lua
@@ -6,12 +6,22 @@
 
 local cache_id = KEYS[1]
 local conf = cmsgpack.unpack(KEYS[2])
-local category = KEYS[3]
+local category = nil
+if KEYS[3] then
+  category = cmsgpack.unpack(KEYS[3])
+end
 cache_id = string.sub(cache_id, 1, conf.cache_elt_len)
 
 local prefix_base = conf.cache_prefix
+
 if category then
-  prefix_base = prefix_base .. "_" .. category
+  -- Compose a deterministic string from the category table
+  local cat_parts = {}
+  for k, v in pairs(category) do
+    table.insert(cat_parts, tostring(k) .. '=' .. tostring(v))
+  end
+  table.sort(cat_parts)
+  prefix_base = prefix_base .. "_cat_" .. table.concat(cat_parts, "_")
 end
 
 -- Try each prefix that is in Redis
