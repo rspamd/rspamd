a6b3606254e577c1fc7afa08487a30bffbd2b576
kevinzb56 <143kevinshah@gmail.com>
2025-06-28 16:32:30 +0530

    add category table to bayes_stat

--- lualib/redis_scripts/bayes_stat.lua
+++ lualib/redis_scripts/bayes_stat.lua
@@ -4,10 +4,28 @@
 -- key2 - symbol to examine
 -- key3 - learn key (e.g. learns_ham or learns_spam)
 -- key4 - max users
--- key5 - (optional) category
+-- key5 - (optional) category table in message pack
 
 local cursor = tonumber(KEYS[1])
+local learn_key = KEYS[3]
 local category = KEYS[5]
+
+local category = nil
+
+if KEYS[5] then
+  category = cmsgpack.unpack(KEYS[5])
+end
+
+local symbol_keys = symbol .. '_keys'
+if category then
+  -- Compose deterministic suffix from the entire table (sorted keys)
+  local cat_parts = {}
+  for k, v in pairs(category) do
+    table.insert(cat_parts, tostring(k) .. '=' .. tostring(v))
+  end
+  table.sort(cat_parts)
+  symbol_keys = symbol_keys .. "_cat_" .. table.concat(cat_parts, "_")
+end
 
 local ret = redis.call('SSCAN', KEYS[2] .. '_keys', cursor, 'COUNT', tonumber(KEYS[4]))
 
