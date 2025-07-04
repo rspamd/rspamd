12a4a9fe1f09a92584d5272a7d748479f5888e31
kevinzb56 <143kevinshah@gmail.com>
2025-07-04 18:19:37 +0530

    updated changes bayes_cache_check

--- lualib/redis_scripts/bayes_cache_check.lua
+++ lualib/redis_scripts/bayes_cache_check.lua
@@ -6,27 +6,17 @@
 
 local cache_id = KEYS[1]
 local conf = cmsgpack.unpack(KEYS[2])
-local category = nil
-if KEYS[3] then
-  category = cmsgpack.unpack(KEYS[3])
-end
+local category = KEYS[3] and cmsgpack.unpack(KEYS[3]) or nil
 cache_id = string.sub(cache_id, 1, conf.cache_elt_len)
-
-local prefix_base = conf.cache_prefix
-
-if category then
-  -- Compose a deterministic string from the category table
-  local cat_parts = {}
-  for k, v in pairs(category) do
-    table.insert(cat_parts, tostring(k) .. '=' .. tostring(v))
-  end
-  table.sort(cat_parts)
-  prefix_base = prefix_base .. "_cat_" .. table.concat(cat_parts, "_")
-end
 
 -- Try each prefix that is in Redis
 for i = 0, conf.cache_max_keys do
-  local prefix = conf.cache_prefix .. string.rep("X", i)
+  local prefix
+  if category then
+    prefix = 'cat_' .. category.id .. '_' .. conf.cache_prefix .. string.rep("X", i)
+  else
+    prefix = conf.cache_prefix .. string.rep("X", i)
+  end
   local have = redis.call('HGET', prefix, cache_id)
 
   if have then
