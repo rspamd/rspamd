19679a36642fc73e70bcf952d1ee6d6c5c76b47a
hunter-nl <junobox@gmail.com>
2025-08-15 16:21:36 +0200

    Update gpt.lua to make use of lua_util.deepcopy function.

--- src/plugins/lua/gpt.lua
+++ src/plugins/lua/gpt.lua
@@ -683,21 +683,7 @@ local function openai_check(task, content, sel_part)
 
   local from_content, url_content = get_meta_llm_content(task)
 
-  -- Deep copy to avoid table reuse between iterations
-  local function table_deep_copy(orig)
-    local copy
-    if type(orig) == 'table' then
-      copy = {}
-      for k, v in pairs(orig) do
-        copy[k] = table_deep_copy(v)
-      end
-    else
-      copy = orig
-    end
-    return copy
-  end
-
-  -- Decide which token length field to use for the given model
+    -- Decide which token length field to use for the given model
   local function get_max_tokens_field(model)
     if not model then
       return 'max_tokens' -- default
@@ -766,7 +752,7 @@ local function openai_check(task, content, sel_part)
       checked = false
     }
     -- Fresh body for each model
-    local body = table_deep_copy(body_base)
+    local body = lua_util.deepcopy(body_base)
 
     -- Set the correct token limit field
     local token_field = get_max_tokens_field(model)
