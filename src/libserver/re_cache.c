rspamd/rspamd

commit 0d62dd6513a5309acfcc19e936b3a7844c83a502

[Fix] Add failsafety for utf8 regexps

src/libserver/re_cache.c
@@ -701,7 +701,7 @@ rspamd_re_cache_process_regexp_data (struct rspamd_re_runtime *rt,
 	re_class = rspamd_regexp_get_class (re);
 
 	if (rt->cache->disable_hyperscan || elt->match_type == RSPAMD_RE_CACHE_PCRE ||
-			!rt->has_hs) {
+			!rt->has_hs || (is_raw && re_class->has_utf8)) {
 		for (i = 0; i < count; i++) {
 			ret = rspamd_re_cache_process_pcre (rt,
 					re,
@@ -943,8 +943,11 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 
 				if (re_class->type == RSPAMD_RE_RAWHEADER) {
 					in = rh->value;
-					raw = TRUE;
 					lenvec[i] = strlen (rh->value);
+
+					if (!g_utf8_validate (in, lenvec[i], NULL)) {
+						raw = TRUE;
+					}
 				}
 				else {
 					in = rh->decoded;
@@ -993,8 +996,11 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 
 				if (re_class->type == RSPAMD_RE_RAWHEADER) {
 					in = rh->value;
-					raw = TRUE;
 					lenvec[i] = strlen (rh->value);
+
+					if (!g_utf8_validate (in, lenvec[i], NULL)) {
+						raw = TRUE;
+					}
 				}
 				else {
 					in = rh->decoded;
@@ -1159,6 +1165,10 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 			if (part->utf_stripped_content) {
 				scvec[i + 1] = (guchar *)part->utf_stripped_content->data;
 				lenvec[i + 1] = part->utf_stripped_content->len;
+
+				if (!IS_PART_UTF (part)) {
+					raw = TRUE;
+				}
 			}
 			else {
 				scvec[i + 1] = (guchar *)"";
@@ -1167,7 +1177,7 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 		}
 
 		ret = rspamd_re_cache_process_regexp_data (rt, re,
-				task, scvec, lenvec, cnt, TRUE);
+				task, scvec, lenvec, cnt, raw);
 		msg_debug_re_task ("checking sa body regexp: %s -> %d",
 				rspamd_regexp_get_pattern (re), ret);
 		g_free (scvec);
@@ -1192,6 +1202,10 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 				if (part->parsed.len > 0) {
 					scvec[i] = (guchar *)part->parsed.begin;
 					lenvec[i] = part->parsed.len;
+
+					if (!IS_PART_UTF (part)) {
+						raw = TRUE;
+					}
 				}
 				else {
 					scvec[i] = (guchar *)"";
@@ -1200,7 +1214,7 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 			}
 
 			ret = rspamd_re_cache_process_regexp_data (rt, re,
-					task, scvec, lenvec, cnt, TRUE);
+					task, scvec, lenvec, cnt, raw);
 			msg_debug_re_task ("checking sa rawbody regexp: %s -> %d",
 					rspamd_regexp_get_pattern (re), ret);
 			g_free (scvec);
@@ -1258,7 +1272,7 @@ rspamd_re_cache_exec_re (struct rspamd_task *task,
 				&lenvec, &cnt)) {
 
 			ret = rspamd_re_cache_process_regexp_data (rt, re,
-					task, scvec, lenvec, cnt, TRUE);
+					task, scvec, lenvec, cnt, raw);
 			msg_debug_re_task ("checking selector (%s) regexp: %s -> %d",
 					re_class->type_data,
 					rspamd_regexp_get_pattern (re), ret);

commit 2540e2784feab21cd22ab1c31dc8d09560aa6200

[Fix] re_cache: Always use raw mode for SARAWBODY regexps
The parsed content is transfer-decoded (base64/QP) but NOT charset-converted,
so it may contain non-UTF-8 data even when IS_TEXT_PART_UTF is true.

Using dynamic raw flag based on IS_TEXT_PART_UTF was incorrect because that
flag indicates whether utf_content is valid UTF-8, not whether parsed content
is valid UTF-8.

Bug introduced in 0d62dd6513 (1.8.3), this restores the original behavior of
always treating SARAWBODY content as raw.

src/libserver/re_cache.c
@@ -1585,6 +1585,9 @@ rspamd_re_cache_exec_re(struct rspamd_task *task,
 		 * encoding, but HTML tags and line breaks will still be present.
 		 * Multiline expressions will need to be used to match strings that are
 		 * broken by line breaks.
+		 *
+		 * Note: parsed content is transfer-decoded but NOT charset-converted,
+		 * so it may contain non-UTF-8 data. Always use raw mode.
 		 */
 		if (MESSAGE_FIELD(task, text_parts)->len > 0) {
 			cnt = MESSAGE_FIELD(task, text_parts)->len;
@@ -1597,10 +1600,6 @@ rspamd_re_cache_exec_re(struct rspamd_task *task,
 				if (text_part->parsed.len > 0) {
 					scvec[i] = (unsigned char *) text_part->parsed.begin;
 					lenvec[i] = text_part->parsed.len;
-
-					if (!IS_TEXT_PART_UTF(text_part)) {
-						raw = TRUE;
-					}
 				}
 				else {
 					scvec[i] = (unsigned char *) "";
@@ -1608,8 +1607,9 @@ rspamd_re_cache_exec_re(struct rspamd_task *task,
 				}
 			}
 
+			/* Always raw - parsed content is not charset-converted */
 			ret = rspamd_re_cache_process_regexp_data(rt, re,
-													  task, scvec, lenvec, cnt, raw, &processed_hyperscan);
+													  task, scvec, lenvec, cnt, TRUE, &processed_hyperscan);
 			msg_debug_re_task("checked sa rawbody regexp: %s -> %d",
 							  rspamd_regexp_get_pattern(re), ret);
 			g_free(scvec);