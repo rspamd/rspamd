87420340263a8c56d26855accfba65bd44aab2c3
Vsevolod Stakhov <vsevolod@rspamd.com>
2026-03-13 14:15:00 +0000

    [Minor] Fix copyright year to 2026

--- src/lua/lua_caseless_table.c
+++ src/lua/lua_caseless_table.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Vsevolod Stakhov
+ * Copyright 2026 Vsevolod Stakhov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
