7ed2baf6f85d481fa8547fcc3124a1a67b7cfa03
Dmitriy Alekseev <1865999+dragoangel@users.noreply.github.com>
2026-05-17 22:58:24 +0200

    [Feature] Update default UA in url_redirector module

--- src/plugins/lua/url_redirector.lua
+++ src/plugins/lua/url_redirector.lua
@@ -36,6 +36,7 @@ local default_ua = {
   'Mozilla/5.0 (compatible; DuckDuckBot-Https/1.1; https://duckduckgo.com/duckduckbot)',
   -- Modern desktop browsers
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0',
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 14.7; rv:150.0) Gecko/20100101 Firefox/150.0',
